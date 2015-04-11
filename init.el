1450dfa7a86a927d870b9481ad75a1c24767d783
Juan Fuentes <jj@xmonk.org>
2015-04-11 10:32:20 -0400

    Had to re-organized the file due to changes in emacs25

--- init.el
+++ init.el
@@ -34,13 +34,30 @@
 (if running-apple
     (setenv "TMPDIR" "/tmp"))
 
+;; Load customizations
+  (cond ((eql system-type 'darwin)
+         (setq custom-file (concat user-emacs-directory "osx-custom.el")))
+        ((eql system-type 'berkeley-unix)
+         (setq custom-file (concat user-emacs-directory "obsd-custom.el")))
+        ((eql system-type 'gnu/linux)
+         (setq custom-file (concat user-emacs-directory "linux-custom.el"))))
+
+(if (file-exists-p custom-file)
+    (load-file custom-file)
+  (message "ERROR: No custom file found or specified"))
+
+;; set exec-path
+(when (eq window-system 'ns)
+  (let ((path (shell-command-to-string "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
+    (setenv "PATH" path)
+    (setq exec-path (split-string path ":"))))
+
 ;; package
 (autoload 'package "package" nil t)
 
 (setq package-archives
       '(("gnu"   . "http://elpa.gnu.org/packages/")
         ("melpa" . "http://melpa.org/packages/")))
-
 
 (package-initialize)
 
@@ -51,37 +68,13 @@
 (defvar use-package-verbose t)
 (require 'use-package)
 
-(use-package exec-path-from-shell
-  :ensure t
-  :commands exec-path-from-shell-initialize
-  :config
-  (dolist (var '("GOROOT" "GOPATH" "OCAML_TOPLEVEL_PATH" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"))
-    (add-to-list 'exec-path-from-shell-variables var)))
-
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-
-;; Load customizations
-(cond ((eql system-type 'darwin)
-       (setq custom-file (concat user-emacs-directory "osx-custom.el")))
-      ((eql system-type 'berkeley-unix)
-       (setq custom-file (concat user-emacs-directory "obsd-custom.el")))
-      ((eql system-type 'gnu/linux)
-       (setq custom-file (concat user-emacs-directory "linux-custom.el"))))
-
-(if (file-exists-p custom-file)
-    (load-file custom-file)
-  (message "ERROR: No custom file found or specified"))
-
 (add-to-list 'load-path (expand-file-name *site-lisp*))
 (mapc (lambda (lib)
         (require lib nil t))
-      '(defuns frames global ido-conf ibuffer-conf
-         elisp-conf c-conf ac-conf smartparens-conf
-         eshell-conf go-conf lisp-conf magit-conf
-         org-conf py-conf emacsd-tile sh-conf helm-conf
-         tramp-conf keymaps aliases pl-conf scala-conf
-         ocaml-conf julia-conf slime-conf nxml-conf))
+      '(defuns global elisp-conf c-conf
+         go-conf lisp-conf magit-conf
+         org-conf py-conf keymaps
+         ocaml-conf))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init.el ends here
