2b20f1f0368aec8384bff2639f0cda6032ceb8f4
Juan Fuentes <jj@xmonk.org>
2015-07-05 11:30:09 -0400

    Load frame and theme

--- site-lisp/global.el
+++ site-lisp/global.el
@@ -34,9 +34,12 @@
       (setq mac-option-modifier 'meta))
   (setq mac-allow-anti-aliasing t))
 
+(if (locate-library "jj-theme")
+	(require 'jj-theme nil t))
+
 ;; set faces
 (set-face-attribute 'default nil :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
-(set-face-attribute 'mode-line nil :font "Lucida Grande" :height 120 :slant 'normal :weight 'normal)
+(set-face-attribute 'mode-line nil :box nil :font "Lucida Grande" :height 120 :slant 'normal :weight 'normal)
 (set-face-attribute 'font-lock-comment-face nil :font "Lucida Grande" :height 130 :slant 'italic :weight 'normal)
 (set-face-attribute 'font-lock-doc-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
 (set-face-attribute 'font-lock-function-name-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
