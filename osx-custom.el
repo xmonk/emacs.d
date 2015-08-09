b7b95f4638ac9fe7e236202be64c4ebba5c2ad11
Juan Fuentes <jj@xmonk.org>
2015-08-09 10:12:14 -0400

    No longer applies.

--- osx-custom.el
+++ osx-custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-emacsclient-executable "/usr/local/bin/emacsclient")
  '(magit-item-highlight-face nil)
  '(package-enable-at-startup nil)
  '(package-selected-packages
