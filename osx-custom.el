2c2f90a556188a5d0c8a543d80212461fed9218a
Juan Fuentes <jj@xmonk.org>
2015-08-21 10:38:49 -0400

    Set default faces.

--- osx-custom.el
+++ osx-custom.el
@@ -1,3 +1,30 @@
+;;; osx-custom.el ---  OS X custom file.
+;;
+;; Filename: osx-custom.el
+;; Description:
+;; Author: Juan Fuentes
+;; Maintainer:
+;; Created: Fri Aug 21 09:14:29 2015 (-0400)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 3
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;
+;;; Commentary:
+;;
+;;
+;;; Change Log:
+;;
+;;
+;;; Code:
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -7,10 +34,20 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (helm-flycheck helm-swoop helm-pydoc helm-projectile ggtags helm projectile go-autocomplete ac-c-headers jedi auto-complete swiper multi-term header2 smex flx-ido smartscan flycheck browse-kill-ring codesearch expand-region flycheck-pyflakes git-timemachine go-eldoc go-errcheck go-mode magit markdown-mode merlin ocp-indent tuareg undo-tree utop whole-line-or-region paredit use-package))))
+    (company-quickhelp company-mode go-company company company-anaconda company-go disaster helm-flycheck helm-swoop helm-pydoc helm-projectile ggtags helm projectile ac-c-headers jedi swiper multi-term header2 smex flx-ido smartscan flycheck browse-kill-ring codesearch expand-region flycheck-pyflakes git-timemachine go-eldoc go-errcheck go-mode magit markdown-mode merlin ocp-indent tuareg undo-tree whole-line-or-region paredit use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(company-preview ((t (:foreground "darkgray" :underline t :font "Lucida Grande Mono" :height 120))))
+ '(company-preview-common ((t (:inherit company-preview))))
+ '(company-scrollbar-bg ((t (:background "lightgray" :foreground "white"))))
+ '(company-scrollbar-fg ((t (:foreground "lightgray" :background "steelblue"))))
+ '(company-tooltip ((t (:background "lightgray" :foreground "black"  :font "Lucida Grande Mono" :height 120))))
+ '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
+ '(company-tooltip-selection ((t (:background "steelblue" :foreground "white"  :font "Lucida Grande Mono" :height 120))))
+ '(eshell-prompt ((t (:inherit default)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; osx-custom.el ends here
