7626bbef4fdcba42a02b0c24ed8a9caf3deac75c
Juan Fuentes <jj@xmonk.org>
2015-08-24 17:52:21 -0400

    update company mode faces

--- osx-custom.el
+++ osx-custom.el
@@ -34,7 +34,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (company-quickhelp company-mode go-company company company-anaconda company-go disaster helm-flycheck helm-swoop helm-pydoc helm-projectile ggtags helm projectile ac-c-headers jedi swiper multi-term header2 smex flx-ido smartscan flycheck browse-kill-ring codesearch expand-region flycheck-pyflakes git-timemachine go-eldoc go-errcheck go-mode magit markdown-mode merlin ocp-indent tuareg undo-tree whole-line-or-region paredit use-package))))
+    (paredit-mode nasm-mode company-quickhelp company-mode go-company company company-anaconda company-go disaster helm-flycheck helm-swoop helm-pydoc helm-projectile ggtags helm projectile ac-c-headers jedi swiper multi-term header2 smex flx-ido smartscan flycheck browse-kill-ring codesearch expand-region flycheck-pyflakes git-timemachine go-eldoc go-errcheck go-mode magit markdown-mode merlin ocp-indent tuareg undo-tree whole-line-or-region paredit use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -44,10 +44,10 @@
  '(company-preview-common ((t (:inherit company-preview))))
  '(company-scrollbar-bg ((t (:background "lightgray" :foreground "white"))))
  '(company-scrollbar-fg ((t (:foreground "lightgray" :background "steelblue"))))
- '(company-tooltip ((t (:background "lightgray" :foreground "black"  :font "Lucida Grande Mono" :height 120))))
+ '(company-tooltip ((t (:background "lightgray" :foreground "black" :font "Lucida Grande Mono" :height 120))))
  '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
  '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
- '(company-tooltip-selection ((t (:background "steelblue" :foreground "white"  :font "Lucida Grande Mono" :height 120))))
+ '(company-tooltip-selection ((t (:background "steelblue" :foreground "white" :font "Lucida Grande Mono" :height 120))))
  '(eshell-prompt ((t (:inherit default)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; osx-custom.el ends here
