9c897d6e3d08f3a16e05f05ffe3b3b432b0e4546
Juan Fuentes <jj@xmonk.org>
2015-08-24 17:54:17 -0400

    add keymap for hexl-find-file

--- site-lisp/keymaps.el
+++ site-lisp/keymaps.el
@@ -28,6 +28,7 @@
   (global-set-key (kbd "C-x C-m") 'execute-extended-command)
   (global-set-key (kbd "C-x m") 'execute-extended-command))
 
+(global-set-key (kbd "C-c C-f") 'hexl-find-file)
 (global-set-key (kbd "C-c m") 'gnus)
 (global-set-key (kbd "M-g") 'goto-line)
 (global-set-key (kbd "C-x k") 'jj/kill-buffer)
