8f492492855fcb5b8a694fbd830a74bd17c10c2c
Juan Fuentes <jj@xmonk.org>
2015-10-17 21:43:17 -0400

    Trying out hydra,and avy

--- site-lisp/global.el
+++ site-lisp/global.el
@@ -189,6 +189,126 @@
   (bind-key "C-. j" 'paredit-join-with-next-list paredit-mode-map)
   (bind-key "C-. J" 'paredit-join-with-previous-list paredit-mode-map))
 
+;;; hydra
+(use-package hydra
+  :ensure t)
+
+;;; avy
+(use-package avy
+  :ensure t)
+
+;;; goto  by John Kitchin
+(defhydra goto (:color blue :hint nil)
+  "
+Goto:
+^Char^              ^Word^                ^org^                    ^search^
+^^^^^^^^---------------------------------------------------------------------------
+_c_: 2 chars        _w_: word by char     _h_: headline in buffer  _o_: helm-occur
+_C_: char           _W_: some word        _a_: heading in agenda   _p_: helm-swiper
+_L_: char in line   _s_: subword by char  _q_: swoop org buffers   _f_: search forward
+^  ^                _S_: some subword     ^ ^                      _b_: search backward
+-----------------------------------------------------------------------------------
+_B_: helm-buffers       _l_: avy-goto-line
+_m_: helm-mini          _i_: ace-window
+_R_: helm-recentf
+
+_n_: Navigate           _._: mark position _/_: jump to mark
+"
+  ("c" avy-goto-char-2)
+  ("C" avy-goto-char)
+  ("L" avy-goto-char-in-line)
+  ("w" avy-goto-word-1)
+  ;; jump to beginning of some word
+  ("W" avy-goto-word-0)
+  ;; jump to subword starting with a char
+  ("s" avy-goto-subword-1)
+  ;; jump to some subword
+  ("S" avy-goto-subword-0)
+
+  ("l" avy-goto-line)
+  ("i" ace-window)
+
+  ("h" helm-org-headlines)
+  ("a" helm-org-agenda-files-headings)
+  ("q" helm-multi-swoop-org)
+
+  ("o" helm-occur)
+  ("p" swiper-helm)
+
+  ("f" isearch-forward)
+  ("b" isearch-backward)
+
+  ("." org-mark-ring-push :color red)
+  ("/" org-mark-ring-goto :color blue)
+  ("B" helm-buffers-list)
+  ("m" helm-mini)
+  ("R" helm-recentf)
+  ("n" hydra-navigate/body))
+
+(global-set-key (kbd "s-g") 'goto/body)
+
+
+;;; window movement from Hydra wiki
+(defhydra hydra-window ()
+  "
+Movement^^        ^Split^         ^Switch^      ^Resize^
+----------------------------------------------------------------
+_h_ ←         _v_ertical      _b_uffer        _q_ X←
+_j_ ↓         _x_ horizontal  _f_ind files    _w_ X↓
+_k_ ↑         _z_ undo        _a_ce 1     _e_ X↑
+_l_ →         _Z_ reset       _s_wap      _r_ X→
+_F_ollow        _D_lt Other     _S_ave      max_i_mize
+_SPC_ cancel    _o_nly this     _d_elete
+"
+  ("h" windmove-left )
+  ("j" windmove-down )
+  ("k" windmove-up )
+  ("l" windmove-right )
+  ("q" hydra-move-splitter-left)
+  ("w" hydra-move-splitter-down)
+  ("e" hydra-move-splitter-up)
+  ("r" hydra-move-splitter-right)
+  ("b" helm-mini)
+  ("f" helm-find-files)
+  ("F" follow-mode)
+  ("a" (lambda ()
+	 (interactive)
+	 (ace-window 1)
+	 (add-hook 'ace-window-end-once-hook
+		   'hydra-window/body))
+   )
+  ("v" (lambda ()
+	 (interactive)
+	 (split-window-right)
+	 (windmove-right))
+   )
+  ("x" (lambda ()
+	 (interactive)
+	 (split-window-below)
+	 (windmove-down))
+   )
+  ("s" (lambda ()
+	 (interactive)
+	 (ace-window 4)
+	 (add-hook 'ace-window-end-once-hook
+		   'hydra-window/body)))
+  ("S" save-buffer)
+  ("d" delete-window)
+  ("D" (lambda ()
+	 (interactive)
+	 (ace-window 16)
+	 (add-hook 'ace-window-end-once-hook
+		   'hydra-window/body))
+   )
+  ("o" delete-other-windows)
+  ("i" ace-maximize-window)
+  ("z" (progn
+	 (winner-undo)
+	 (setq this-command 'winner-undo))
+   )
+  ("Z" winner-redo)
+  ("SPC" nil))
+(global-set-key (kbd "M-g") #'hydra-window/body)
 
 ;;; diminish
 (use-package diminish
@@ -327,7 +447,7 @@
 ;;; swiper
 (use-package swiper
   :ensure t
-  :bind ("C-c C-s" . swiper))
+  :bind ("C-s" . swiper))
 
 ;;; winner mode
 (use-package winner
