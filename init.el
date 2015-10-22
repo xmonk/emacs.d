27547e520b9e43957cefe4d3f162d137574a5343
Juan Fuentes <jj@xmonk.org>
2015-10-22 17:06:41 -0400

    Trying a dark background for a change.

--- init.el
+++ init.el
@@ -50,7 +50,7 @@
   (setq mac-allow-anti-aliasing t))
 
 ;; set faces
-(set-face-attribute 'default nil :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
+(set-face-attribute 'default nil :background "black" :foreground "#D0D0D0" :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
 (set-face-attribute 'mode-line nil :box nil :font "Lucida Grande" :height 120 :slant 'normal :weight 'normal)
 (set-face-attribute 'font-lock-comment-face nil :font "Lucida Grande" :height 130 :slant 'italic :weight 'normal)
 (set-face-attribute 'font-lock-doc-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
