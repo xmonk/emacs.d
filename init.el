c8c45f942203f71b85ec69d491fd96fb4bfe18da
Juan Fuentes <jj@xmonk.org>
2015-12-09 07:01:00 -0400

    Don't use italic for comments.

--- init.el
+++ init.el
@@ -55,7 +55,7 @@
 ;; (set-face-attribute 'cursor nil :background "#D0D0D0" :foreground "#D0D0D0")
 (set-face-attribute 'default nil :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
 (set-face-attribute 'mode-line nil :box nil :font "Lucida Grande" :height 120 :slant 'normal :weight 'normal)
-(set-face-attribute 'font-lock-comment-face nil :font "Lucida Grande" :height 130 :slant 'italic :weight 'normal)
+(set-face-attribute 'font-lock-comment-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
 (set-face-attribute 'font-lock-doc-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
 (set-face-attribute 'font-lock-function-name-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
 
