9a3e46f45895da388fde262a083d3b047ba2fd12
Juan Fuentes <jj@xmonk.org>
2016-01-24 19:35:56 -0400

    disabled syntax highlighting

--- osx-custom.el
+++ osx-custom.el
@@ -30,6 +30,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(font-lock-mode nil)
+ '(global-font-lock-mode nil)
  '(magit-item-highlight-face nil)
  '(package-enable-at-startup nil)
  '(package-selected-packages
