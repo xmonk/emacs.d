40c217485a606f93d87f34b0a30290e8330ebde7
Juan Fuentes <jj@xmonk.org>
2016-03-02 10:02:30 -0400

    Fix issue with utop failing to start in emacs mode

--- site-lisp/ocaml-conf.el
+++ site-lisp/ocaml-conf.el
@@ -42,7 +42,9 @@
     :ensure t
     :config
     ;; Automatically load utop.el
-    (autoload 'utop "utop" "Toplevel for OCaml" t))
+    (autoload 'utop "utop" "Toplevel for OCaml" t)
+    (autoload 'utop-minor-mode "utop" "Minor mode for utop" t)
+    (add-hook 'tuareg-mode-hook 'utop-minor-mode))
 
   (use-package ocp-indent
     :ensure t
