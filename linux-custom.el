98a5416c1b3ce8bf527b1a228d51bf9d0c06759a
Juan Fuentes <jj@xmonk.org>
2016-03-12 15:10:56 -0400

    Add company color configuration

--- linux-custom.el
+++ linux-custom.el
@@ -32,13 +32,20 @@
  '(frame-background-mode 'dark)
  '(magit-item-highlight-face nil)
  '(package-enable-at-startup nil)
- '(package-selected-packages (quote (use-package)))
- '(temporary-file-directory "/tmp"))
+ '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-preview ((t (:foreground "darkgray" :underline t :font "Lucida Grande Mono" :height 120))))
+ '(company-preview-common ((t (:inherit company-preview))))
+ '(company-scrollbar-bg ((t (:background "lightgray" :foreground "white"))))
+ '(company-scrollbar-fg ((t (:foreground "lightgray" :background "steelblue"))))
+ '(company-tooltip ((t (:background "lightgray" :foreground "black" :font "Lucida Grande Mono" :height 120))))
+ '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
+ '(company-tooltip-selection ((t (:background "steelblue" :foreground "white" :font "Lucida Grande Mono" :height 120))))
  '(eshell-prompt ((t (:inherit default)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
