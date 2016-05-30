b388c50122fdb0dd78f7d645992e3765d7c0b404
Juan Fuentes <jj@xmonk.org>
2016-05-30 13:52:51 -0400

    Clean up.
    Unified some code snippet that were sparsed around the file.

--- init.el
+++ init.el
@@ -33,8 +33,6 @@
 (defvar *vendor* (concat user-emacs-directory "vendor/")
   "Location of third-party files to be loaded at start up.")
 
-(defvar running-apple (string-match "apple" system-configuration))
-
 ;; 20MB let's see how well it works.
 (setq gc-cons-threshold 20000000)
 
@@ -44,14 +42,24 @@
 (dolist (mode '(scroll-bar-mode tool-bar-mode))
   (if (fboundp mode) (funcall mode -1)))
 
-(when running-apple
+(when (memq window-system '(mac ns))
   (if (boundp 'mac-option-modifier)
       (setq mac-option-modifier 'meta))
-  (setq mac-allow-anti-aliasing t))
+  (setq mac-allow-anti-aliasing t)
+  (setenv "TMPDIR" "/tmp") ;; os x sets it to /var/tmp/...
+  (let ((path (shell-command-to-string "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
+    (setenv "PATH" path)
+    (setq exec-path (split-string path path-separator)))
 
-;; In os x there is an issue with tramp and TMPDIR, this is a work around.
-(if running-apple
-    (setenv "TMPDIR" "/tmp"))
+  ;; set faces
+  ;; Uncomment following two lines and comment the third for dark background
+  ;; (set-face-attribute 'default nil :background "black" :foreground "#D0D0D0" :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
+  ;; (set-face-attribute 'cursor nil :background "#D0D0D0" :foreground "#D0D0D0")
+  (set-face-attribute 'default nil :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
+  (set-face-attribute 'mode-line nil :box nil :font "Lucida Grande" :height 120 :slant 'normal :weight 'normal)
+  (set-face-attribute 'font-lock-comment-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
+  (set-face-attribute 'font-lock-doc-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
+  (set-face-attribute 'font-lock-function-name-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal))
 
 ;; Load customization's
 (cond ((eql system-type 'darwin)
@@ -65,20 +73,10 @@
     (load-file custom-file)
   (message "ERROR: No custom file found or specified"))
 
-;; set exec-path
-(when (eq window-system 'ns)
-  (let ((path (shell-command-to-string "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
-    (setenv "PATH" path)
-    (setq exec-path (split-string path path-separator))))
-
 ;;; Server
 (require 'server nil t)
 (when (and (>= emacs-major-version 23) (not (server-running-p)))
   (server-start))
-
-;;; This fixes the issue of 'url-insert-buffer-content not being autoloaded.
-;; (unless (fboundp 'url-insert-buffer-contents)
-;;   (load-library "url-handlers"))
 
 ;; package
 (autoload 'package "package" nil t)
@@ -98,12 +96,10 @@
 (defvar use-package-verbose nil)
 
 (add-to-list 'load-path (expand-file-name *site-lisp*))
-(use-package jj-dark-theme :disabled)
 (use-package jj-theme)
 (use-package defuns)
 (use-package global)
 (use-package keymaps)
-;; (use-package helm-conf :disabled)
 (use-package elisp-conf)
 (use-package lisp-conf)
 (use-package c-conf)
@@ -111,19 +107,9 @@
 (use-package org-conf)
 (use-package magit-conf)
 (use-package py-conf)
-(use-package ocaml-conf)
+;;(use-package ocaml-conf)
 (use-package rust-conf)
 ;; (use-package java-conf)
-
-;; set faces
-;; Uncomment following two lines and comment the third for dark background
-;; (set-face-attribute 'default nil :background "black" :foreground "#D0D0D0" :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
-;; (set-face-attribute 'cursor nil :background "#D0D0D0" :foreground "#D0D0D0")
-(set-face-attribute 'default nil :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
-(set-face-attribute 'mode-line nil :box nil :font "Lucida Grande" :height 120 :slant 'normal :weight 'normal)
-(set-face-attribute 'font-lock-comment-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
-(set-face-attribute 'font-lock-doc-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
-(set-face-attribute 'font-lock-function-name-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init.el ends here
