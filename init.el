38dd5f43991567ece92bb3dd152e60fa8ddf5033
Juan Fuentes <jj@xmonk.org>
2016-06-01 07:19:13 -0400

    cleaning and getting load ordering right

--- init.el
+++ init.el
@@ -34,26 +34,6 @@
 
 ;; 20MB let's see how well it works.
 (setq gc-cons-threshold 20000000)
-
-(when (memq window-system '(mac ns))
-  (if (boundp 'mac-option-modifier)
-      (setq mac-option-modifier 'meta))
-  (setq mac-allow-anti-aliasing t)
-  (setenv "TMPDIR" "/tmp") ;; os x sets it to /var/tmp/...
-  (let ((path (shell-command-to-string "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
-    (setenv "PATH" path)
-    (setq exec-path (split-string path path-separator)))
-
-  ;; set faces
-  ;; Uncomment following two lines and comment the third for dark background
-  (set-face-attribute 'default nil :background "black" :foreground "#D0D0D0" :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
-  ;; (set-face-attribute 'cursor nil :background "#D0D0D0" :foreground "#D0D0D0")
-  ;; (set-face-attribute 'default nil :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
-  (set-face-attribute 'mode-line nil :box nil :font "Lucida Grande" :height 120 :slant 'normal :weight 'normal)
-  (set-face-attribute 'font-lock-comment-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
-  (set-face-attribute 'font-lock-doc-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
-  (set-face-attribute 'font-lock-function-name-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal))
-
 (unless window-system
   (menu-bar-mode -1))
 
@@ -71,6 +51,26 @@
 (if (file-exists-p custom-file)
     (load-file custom-file)
   (message "ERROR: No custom file found or specified"))
+
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes"))
+
+(when (memq window-system '(mac ns))
+  (if (boundp 'mac-option-modifier)
+      (setq mac-option-modifier 'meta))
+  (setq mac-allow-anti-aliasing t)
+  (setenv "TMPDIR" "/tmp") ;; os x sets it to /var/tmp/...
+  (let ((path (shell-command-to-string "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
+    (setenv "PATH" path)
+    (setq exec-path (split-string path path-separator)))
+
+  ;; set faces
+  ;; Uncomment following two lines and comment the third for dark background
+  ;; (set-face-attribute 'default nil :background "black" :foreground "#D0D0D0" :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
+  (set-face-attribute 'default nil :font "Lucida Grande Mono" :height 120 :slant 'normal :weight 'normal)
+  (set-face-attribute 'mode-line nil :box nil :font "Lucida Grande" :height 120 :slant 'normal :weight 'normal)
+  (set-face-attribute 'font-lock-comment-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
+  (set-face-attribute 'font-lock-doc-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal)
+  (set-face-attribute 'font-lock-function-name-face nil :font "Lucida Grande" :height 130 :slant 'normal :weight 'normal))
 
 ;;; Server
 (require 'server nil t)
@@ -95,7 +95,6 @@
 (defvar use-package-verbose nil)
 
 (add-to-list 'load-path (expand-file-name *site-lisp*))
-(use-package jj-dark-theme)
 (use-package defuns)
 (use-package global)
 (use-package keymaps)
