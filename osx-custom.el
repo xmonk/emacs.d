508c0bfbf2497a550840078950770cdb4d314b2a
Juan Fuentes <jj@xmonk.org>
2016-06-13 19:35:18 -0400

    Remove old packages and themes

--- osx-custom.el
+++ osx-custom.el
@@ -30,18 +30,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("b7ba8bd70d2c954e326144c5bf11eecffd55683dfa76aa16bc53572a6184bc1d" default)))
  '(font-lock-maximum-decoration t)
  '(font-lock-mode t t)
  '(global-font-lock-mode t)
  '(indent-tabs-mode t)
  '(magit-item-highlight-face nil)
  '(package-enable-at-startup nil)
- '(package-selected-packages
-   (quote
-    (ujelly-theme slime slime-company racer company-go rustfmt rust-fmt rust-mode flycheck-ocaml c-eldoc smex xcscope go-guru web-mode ob-go anaconda-mode flycheck company org-plus-contrib org go-rename go-mode direx whole-line-or-region utop use-package undo-tree tuareg swiper smartscan projectile paredit ocp-indent merlin markdown-mode magit hydra header2 go-errcheck go-eldoc git-timemachine flycheck-pyflakes expand-region company-anaconda codesearch browse-kill-ring)))
  '(temporary-file-directory "/tmp"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
