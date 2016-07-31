3f1a82148da5cc9fcfce0fdcbe927d05fef1feae
Juan Fuentes <jj@xmonk.org>
2016-07-31 09:24:43 -0400

    Convert to a proper theme

--- themes/jj-dark-theme.el
+++ themes/jj-dark-theme.el
@@ -24,73 +24,82 @@
 ;;
 ;;; Code:
 
-(deftheme jj-dark "My personal theme")
-  (custom-theme-set-faces 'jj-dark
-   '(default ((t (:foreground "#D0D0D0" :background "black"))))
-   '(cursor ((t (:background "#D0D0D0" :foreground "#D0D0D0"))))
-   '(mode-line ((t (:foreground "black" :background "white" :box nil :font "Lucida Grande" :height 120 :slant normal :weight normal))))
-   '(mode-line-inactive ((t (:foreground "#D0D0D0" :background "black" :box nil :font "Lucida Grande" :height 120 :slant normal :weight light))))
-   '(linum ((t (:stipple nil :background "black" :foreground "#D0D0D0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 92 :width normal :family "Lucida Grande"))))
-   '(font-lock-builtin-face ((t (:foreground "#D0D0D0" :bold nil ))))
-   '(font-lock-comment-face ((t (:foreground "#D0D0D0" :slant italic :weight normal ))))
-   '(font-lock-comment-delimiter-face ((t (:foreground "#D0D0D0" :slant normal))))
-   '(font-lock-function-name-face ((t (:foreground "#D0D0D0" :bold nil))))
-   '(font-lock-keyword-face ((t (:foreground "#D0D0D0" :weight bold))))
-   '(font-lock-preprocessor-face ((t (:foreground "#D0D0D0" :bold nil))))
-   '(font-lock-regexp-grouping-backslash ((t (:foreground "#D0D0D0"))))
-   '(font-lock-regexp-grouping-construct ((t (:foreground "#D0D0D0"))))
-   '(font-lock-string-face ((t (:foreground "#D0D0D0" ))))
-   '(font-lock-type-face ((t (:foreground "#D0D0D0" ))))
-   '(font-lock-constant-face ((t (:foreground "#D0D0D0" ))))
-   '(font-lock-variable-name-face ((t (:foreground "#D0D0D0" ))))
-   '(minibuffer-prompt ((t (:foreground "#D0D0D0" :bold t ))))
-   '(org-code ((t (:foreground "#D0D0D0" :weight bold))))
-   '(org-verbatim ((t (:foreground "#D0D0D0"))))
-   '(org-agenda-dimmed-todo-face ((t (:inherit default :weight bold))))
-   '(org-agenda-done ((t (:inherit nil :strike-through t))))
-   '(org-done ((t (:weight bold :strike-through t))))
-   '(org-headline-done ((((class color) (min-colors 16) (background light)) (:weigth bold :strike-through t))))
-   '(org-todo ((t (:inherit default :weight bold))))
-   '(ido-subdir ((t (:weight bold))))
+;; #D0D0D0
+
+(deftheme jj-dark "Personal dark theme")
+(let ((class '((class color)
+	       (min-colors 89)))
+      (fg1 "#C7C7C7")
+      (fg2 "#1D1F21")
+      (bg1 "#00000")
+      (bg2 "#1a1a1a"))
+
+  (custom-theme-set-faces
+   'jj-dark
+   `(default ((,class (:foreground ,fg1 :background ,bg2))))
+   `(cursor ((,class (:background ,bg2 :foreground ,fg1))))
+   `(mode-line ((,class (:foreground ,fg2 :background "white" :box nil :font "Lucida Grande" :height 120 :slant normal :weight normal))))
+   `(mode-line-inactive ((,class (:foreground ,fg1 :background ,bg1 :box nil :font "Lucida Grande" :height 120 :slant normal :weight light))))
+   `(linum ((,class (:stipple nil :background ,bg1 :foreground ,fg1 :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 92 :width normal :family "Lucida Grande"))))
+   `(font-lock-builtin-face ((,class (:foreground ,fg1 :bold nil ))))
+   `(font-lock-comment-face ((,class (:foreground ,fg1 :slant italic :weight normal ))))
+   `(font-lock-comment-delimiter-face ((,class (:foreground ,fg1 :slant normal))))
+   `(font-lock-function-name-face ((,class (:foreground ,fg1 :bold nil))))
+   `(font-lock-keyword-face ((,class (:foreground ,fg1 :weight bold))))
+   `(font-lock-preprocessor-face ((,class (:foreground ,fg1 :bold nil))))
+   `(font-lock-regexp-grouping-backslash ((,class (:foreground ,fg1))))
+   `(font-lock-regexp-grouping-construct ((,class (:foreground ,fg1))))
+   `(font-lock-string-face ((,class (:foreground ,fg1 ))))
+   `(font-lock-type-face ((,class (:foreground ,fg1 ))))
+   `(font-lock-constant-face ((,class (:foreground ,fg1 ))))
+   `(font-lock-variable-name-face ((,class (:foreground ,fg1 ))))
+   `(minibuffer-prompt ((,class (:foreground ,fg1 :bold t ))))
+   `(org-code ((,class (:foreground ,fg1 :weight bold))))
+   `(org-verbatim ((,class (:foreground ,fg1))))
+   `(org-agenda-dimmed-todo-face ((,class (:inherit default :weight bold))))
+   `(org-agenda-done ((,class (:inherit nil :strike-through t))))
+   `(org-done ((,class (:weight bold :strike-through t))))
+   `(org-headline-done ((((class color) (min-colors 16) (background light)) (:weigth bold :strike-through t))))
+   `(org-todo ((,class (:inherit default :weight bold))))
+   `(ido-subdir ((,class (:weight bold))))
    ;;; magit
-   '(magit-item-highlight-face ((t (:inherit default))))
-   '(magit-dimmed ((t (:inherit default))))
-   '(magit-diff-hunk-heading ((t (:inherit default))))
-   '(magit-diff-hunk-heading-highlight ((t (:inherit default))))
-   '(magit-diff-added ((t (:inherit default))))
-   '(magit-diff-added-highlight ((t (:inherit default))))
-   '(magit-diff-removed ((t (:inherit default))))
-   '(magit-diff-removed-highlight ((t (:inherit default))))
+   `(magit-item-highlight-face ((,class (:inherit default))))
+   `(magit-dimmed ((,class (:inherit default))))
+   `(magit-diff-hunk-heading ((,class (:inherit default))))
+   `(magit-diff-hunk-heading-highlight ((,class (:inherit default))))
+   `(magit-diff-added ((,class (:inherit default))))
+   `(magit-diff-added-highlight ((,class (:inherit default))))
+   `(magit-diff-removed ((,class (:inherit default))))
+   `(magit-diff-removed-highlight ((,class (:inherit default))))
    ;;; auto-complete
-   '(ac-completion-face ((t (:background "black" :foreground "#D0D0D0"))))
-   '(ac-candidate-face ((t (:background "black" :foreground "#D0D0D0"))))
-   '(ac-selection-face ((t (:background "#D0D0D0" :foreground "black" :bold t ))))
-   '(popup-face ((t (:inherit default))))
-   '(popup-summary-face ((t (:inherit default))))
-   '(popup-menu-face ((t (:inherit default))))
-   '(popup-tip-face ((t (:background "#D0D0D0" :foreground "black" :bold t :font "Lucida Grande Mono Nrw" :height 120))))
+   `(ac-completion-face ((,class (:background ,bg1 :foreground ,fg1))))
+   `(ac-candidate-face ((,class (:background ,bg1 :foreground ,fg1))))
+   `(ac-selection-face ((,class (:background ,fg1 :foreground ,bg1 :bold t ))))
+   `(popup-face ((,class (:inherit default))))
+   `(popup-summary-face ((,class (:inherit default))))
+   `(popup-menu-face ((,class (:inherit default))))
+   `(popup-tip-face ((,class (:background ,fg1 :foreground ,bg1 :bold t :font "Lucida Grande Mono Nrw" :height 120))))
    ;;; company
-   '(company-preview ((t (:foreground "#D0D0D0" :underline t :font "Lucida Grande Mono Nrw" :height 120))))
-   '(company-preview-common ((t (:inherit company-preview))))
-   '(company-scrollbar-bg ((t (:background "black" :foreground "#D0D0D0"))))
-   '(company-scrollbar-fg ((t (:background "#D0D0D0" :foreground "black"))))
-   '(company-tooltip ((t (:background "black" :foreground "#D0D0D0" :font "Lucida Grande Mono" :height 120))))
-   '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
-   '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
-   '(company-tooltip-selection ((t (:background "#D0D0D0" :foreground "black" :font "Lucida Grande Mono" :height 120))))
+   `(company-preview ((,class (:foreground ,fg1 :underline t :font "Lucida Grande Mono Nrw" :height 120))))
+   `(company-preview-common ((,class (:inherit company-preview))))
+   `(company-scrollbar-bg ((,class (:background ,bg2 :foreground ,fg1))))
+   `(company-scrollbar-fg ((,class (:background ,fg1 :foreground ,bg2))))
+   `(company-tooltip ((,class (:background ,bg2 :foreground ,fg1 :font "Lucida Grande Mono" :height 120))))
+   `(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
+   `(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
+   `(company-tooltip-selection ((,class (:background ,fg1 :foreground ,bg2 :font "Lucida Grande Mono" :height 120))))
    ;;; eshell
-   '(eshell-prompt ((t (:inherit default))))
-   '(font-lock-warning-face ((t (:foreground "black" :bold t :underline t)))))
+   `(eshell-prompt ((,class (:inherit default))))
+   `(font-lock-warning-face ((,class (:foreground ,bg1 :bold t :underline t))))))
 
 ;;; ###autoload
-(and load-file-name
-    (boundp 'custom-theme-load-path)
-    (add-to-list 'custom-theme-load-path
-                 (file-name-as-directory
-                  (file-name-directory load-file-name))))
-;; Automatically add this theme to the load path
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+	       (file-name-as-directory (file-name-directory load-file-name))))
 
 (provide-theme 'jj-dark)
-
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; jj-dark-theme.el ends here
