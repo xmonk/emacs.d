04b92304a3341dc1793cc0a2f092d117eccff531
Juan Fuentes <jj@xmonk.org>
2016-10-16 13:53:26 -0400

    set default look for company-mode

--- osx-custom.el
+++ osx-custom.el
@@ -37,13 +37,21 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (doom-themes magit-popup git-commit with-editor async pkg-info epl pythonic f s dash caml flx company-flx swift-mode org tuareg workgroups2 php-mode elpy nasm-mode jedi rust-mode go-guru go-rename company-go go-errcheck go-eldoc anaconda-mode flycheck company ob-go xcscope whole-line-or-region web-mode use-package undo-tree smartscan py-yapf projectile paredit markdown-mode magit go-mode git-timemachine flycheck-pyflakes expand-region company-anaconda codesearch browse-kill-ring)))
+    (ggtags magit-popup git-commit with-editor async pkg-info epl pythonic f s dash caml flx company-flx swift-mode org tuareg workgroups2 php-mode elpy nasm-mode jedi rust-mode go-guru go-rename company-go go-errcheck go-eldoc anaconda-mode flycheck company ob-go xcscope whole-line-or-region web-mode use-package undo-tree smartscan py-yapf projectile paredit markdown-mode magit go-mode git-timemachine flycheck-pyflakes expand-region company-anaconda codesearch browse-kill-ring)))
  '(temporary-file-directory "/tmp"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-preview ((t (:background "black" :foreground "white" :underline t :font "Lucida Grande Mono" :height 120))))
+ '(company-preview-common ((t (:inherit company-preview))))
+ '(company-scrollbar-bg ((t (:background "white" :foreground "black"))))
+ '(company-scrollbar-fg ((t (:foreground "white" :background "black"))))
+ '(company-tooltip ((t (:background "black" :foreground "white" :font "Lucida Grande Mono" :height 120))))
+ '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
+ '(company-tooltip-selection ((t (:background "white" :foreground "#1D1D1D" :font "Lucida Grande Mono" :height 120))))
  '(sp-pair-overlay-face ((t nil))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; osx-custom.el ends here
