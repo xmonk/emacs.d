319f731a265298fa5496891dbc1d93f8d8d2ea3f
Juan Fuentes <jj@xmonk.org>
2016-11-30 15:32:54 -0400

    Set gnutls trustfiles

--- osx-custom.el
+++ osx-custom.el
@@ -32,26 +32,21 @@
  '(custom-safe-themes
    (quote
     ("86450e783c6b9ba009e53aa7eaf647468c09bf3463482c58da66b1512a6cb2dc" "aa25660125463fed1b90638b816fbc26568a27bd8278d39f54df4bfc82e3bf1e" default)))
+ '(gnutls-trustfiles
+   (quote
+    ("/etc/ssl/certs/ca-certificates.crt" "/etc/pki/tls/certs/ca-bundle.crt" "/etc/ssl/ca-bundle.pem" "/usr/ssl/certs/ca-bundle.crt" "/usr/local/share/certs/ca-root-nss.crt" "/usr/local/etc/openssl/cert.pem" "~/.certs/ca-certificates.pem")))
  '(indent-tabs-mode t)
  '(magit-item-highlight-face nil)
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (ggtags magit-popup git-commit with-editor async pkg-info epl pythonic f s dash caml flx company-flx swift-mode org workgroups2 php-mode elpy nasm-mode jedi go-guru go-rename company-go go-errcheck go-eldoc anaconda-mode flycheck company ob-go xcscope whole-line-or-region web-mode use-package undo-tree smartscan py-yapf projectile paredit markdown-mode magit go-mode git-timemachine flycheck-pyflakes expand-region company-anaconda codesearch browse-kill-ring)))
+    (multi-term company-c-headers pyenv-mode py-autopep8 pip-requirements ir-black-theme ggtags magit-popup git-commit with-editor async pkg-info epl pythonic f s dash caml flx company-flx swift-mode org workgroups2 php-mode elpy nasm-mode jedi go-guru go-rename company-go go-errcheck go-eldoc anaconda-mode flycheck company ob-go xcscope whole-line-or-region web-mode use-package undo-tree smartscan py-yapf projectile paredit markdown-mode magit go-mode git-timemachine flycheck-pyflakes expand-region company-anaconda codesearch browse-kill-ring)))
  '(temporary-file-directory "/tmp"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-preview ((t (:background "black" :foreground "white" :underline t :font "Lucida Grande Mono" :height 120))))
- '(company-preview-common ((t (:inherit company-preview))))
- '(company-scrollbar-bg ((t (:background "white" :foreground "black"))))
- '(company-scrollbar-fg ((t (:foreground "white" :background "black"))))
- '(company-tooltip ((t (:background "black" :foreground "white" :font "Lucida Grande Mono" :height 120))))
- '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
- '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
- '(company-tooltip-selection ((t (:background "white" :foreground "#1D1D1D" :font "Lucida Grande Mono" :height 120))))
  '(sp-pair-overlay-face ((t nil))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; osx-custom.el ends here
