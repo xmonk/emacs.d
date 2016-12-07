1081c6f39c66081831620477a7b7c662471076ef
Juan Fuentes <jj@xmonk.org>
2016-12-07 10:41:14 -0400

    new packages

--- osx-custom.el
+++ osx-custom.el
@@ -40,7 +40,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (multi-term company-c-headers pyenv-mode py-autopep8 pip-requirements ir-black-theme ggtags magit-popup git-commit with-editor async pkg-info epl pythonic f s dash caml flx company-flx swift-mode org workgroups2 php-mode elpy nasm-mode jedi go-guru go-rename company-go go-errcheck go-eldoc anaconda-mode flycheck company ob-go xcscope whole-line-or-region web-mode use-package undo-tree smartscan py-yapf projectile paredit markdown-mode magit go-mode git-timemachine flycheck-pyflakes expand-region company-anaconda codesearch browse-kill-ring)))
+    (vcl-mode multi-term company-c-headers pyenv-mode py-autopep8 pip-requirements ir-black-theme ggtags magit-popup git-commit with-editor async pkg-info epl pythonic f s dash caml flx company-flx swift-mode org workgroups2 php-mode elpy nasm-mode jedi go-guru go-rename company-go go-errcheck go-eldoc anaconda-mode flycheck company ob-go xcscope whole-line-or-region web-mode use-package undo-tree smartscan py-yapf projectile paredit markdown-mode magit go-mode git-timemachine flycheck-pyflakes expand-region company-anaconda codesearch browse-kill-ring)))
  '(temporary-file-directory "/tmp"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
