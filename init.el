cb1d014b8809685eb7fd6cf30d8337869679d0d1
Juan Fuentes <jj@xmonk.org>
2017-04-19 21:33:01 -0400

    Reorganized for readability.

--- init.el
+++ init.el
@@ -23,13 +23,14 @@
 ;;
 ;;
 ;;; Code:
+
 (autoload 'debug "debug" "emacs debugger")
 (setq debug-on-error nil)
 
 ;; set faces
 ;; themes
 (add-to-list 'custom-theme-load-path (expand-file-name (concat user-emacs-directory "themes/")))
-(set-face-attribute 'default nil :background "#000000" :foreground "#C7C7C7" :font "SF Mono" :height 130 :slant 'normal :weight 'normal)
+(set-face-attribute 'default nil :background "#000000" :foreground "#C7C7C7" :font "SF Mono Medium" :height 130 :slant 'normal :weight 'normal)
 
 (defvar *site-lisp* (concat user-emacs-directory "site-lisp/")
   "Location of configuration files to be loaded at start up.")
@@ -39,39 +40,24 @@
 
 ;; 20MB let's see how well it works.
 (setq gc-cons-threshold 20000000)
+
 (unless window-system
   (menu-bar-mode -1))
 
-(dolist (mode '(scroll-bar-mode tool-bar-mode))
-  (if (fboundp mode) (funcall mode -1)))
-
 ;; frame
 (when (memq window-system '(mac ns))
-  (if (boundp 'mac-option-modifier)
-      (setq mac-option-modifier 'meta))
-  (if (boundp 'mac-command-modifier)
-      (setq mac-command-modifier 'super))
+  (dolist (mode '(scroll-bar-mode tool-bar-mode))
+    (if (fboundp mode) (funcall mode -1)))
+
+  (cond ((boundp 'mac-option-modifier)
+	 (setq mac-option-modifier 'meta))
+	((boundp 'mac-command-modifier)
+	 (setq mac-command-modifier 'super)))
   (setq mac-allow-anti-aliasing t)
   (setenv "TMPDIR" "/tmp") ;; os x sets it to /var/tmp/...
   (let ((path (shell-command-to-string "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
     (setenv "PATH" path)
     (setq exec-path (split-string path path-separator))))
-
-;; package
-(autoload 'package "package" nil t)
-
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")
-        ("org" . "http://orgmode.org/elpa/")))
-
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(require 'use-package)
-(defvar use-package-verbose t)
 
 ;; Load customization's
 (cond ((eql system-type 'darwin)
@@ -94,13 +80,29 @@
 (when (and (>= emacs-major-version 23) (not (server-running-p)))
   (server-start))
 
+;; package
+(autoload 'package "package" nil t)
+
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+ (require 'use-package))
+(defvar use-package-verbose nil)
+
 (add-to-list 'load-path (expand-file-name *site-lisp*))
 (use-package defuns
   :config
   (init-maxframe))
 (use-package global)
 (use-package keymaps)
-(use-package cedet-conf)
+(use-package cedet-conf :defer t)
 (use-package elisp-conf)
 (use-package lisp-conf)
 (use-package magit-conf)
@@ -110,6 +112,7 @@
 (use-package py-conf)
 (use-package eshell-conf)
 (use-package ocaml-conf
+  :disabled
   :if (file-directory-p (concat (getenv "HOME") "/.opam"))
   :config
   (load (concat (getenv "HOME") "/.opam/system/share/emacs/site-lisp/tuareg-site-file")))
