0d8aaf02261005a0b4879d64a8698e252b6cbae5
Juan Fuentes <jj@xmonk.org>
2017-04-20 10:26:30 -0400

    Cleanup themes

--- osx-custom.el
+++ osx-custom.el
@@ -29,9 +29,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("372b627d8f8998d2d7c6a50d3aba91d8535c2f8dfd265288ea7dcc5374752b27" "60a16cd1aeabad334e8836be393582eabb0e931f34ae80f064ff2306de864f46" "86450e783c6b9ba009e53aa7eaf647468c09bf3463482c58da66b1512a6cb2dc" "aa25660125463fed1b90638b816fbc26568a27bd8278d39f54df4bfc82e3bf1e" default)))
  '(flycheck-python-pyflakes-executable "pyflakes")
  '(gnutls-trustfiles
    (quote
