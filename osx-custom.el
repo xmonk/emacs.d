3318eecfe83bd44f6d4830ae576d8a3bbd60149b
Juan Fuentes <jj@xmonk.org>
2017-04-26 16:16:02 -0400

    don't load my company faces if a custom theme is used

--- osx-custom.el
+++ osx-custom.el
@@ -38,21 +38,22 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (company-flx company-go flycheck-pyflakes-executable flycheck-flak anaconda-mode company-anaconda company go-mode flycheck rust-mode rustfmt ob-go xcscope whole-line-or-region web-mode use-package undo-tree py-yapf projectile php-mode paredit markdown-mode magit go-rename go-guru go-errcheck go-eldoc git-timemachine ggtags flycheck-pyflakes expand-region codesearch browse-kill-ring)))
+    (go-guru go-rename company-go go-errcheck go-eldoc color-theme-sanityinc-tomorrow company-anaconda anaconda-mode py-yapf go-mode magit flycheck browse-kill-ring whole-line-or-region codesearch expand-region web-mode markdown-mode projectile undo-tree git-timemachine xcscope paredit company-flx company use-package)))
  '(temporary-file-directory "/tmp"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-preview ((t (:background "blue" :foreground "grey"))))
- '(company-preview-common ((t (:inherit company-preview))))
- '(company-scrollbar-bg ((t (:background "grey" :foreground "black"))))
- '(company-scrollbar-fg ((t (:foreground "grey" :background "black"))))
- '(company-tooltip ((t (:background "black" :foreground "grey" :font "SF Mono" :height 120))))
- '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
- '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
- '(company-tooltip-selection ((t (:background "grey" :foreground "#1D1D1D" :font "SF Mono" :height 120))))
+ (unless custom-enabled-themes
+   '(company-preview ((t (:background "blue" :foreground "grey"))))
+   '(company-preview-common ((t (:inherit company-preview))))
+   '(company-scrollbar-bg ((t (:background "grey" :foreground "black"))))
+   '(company-scrollbar-fg ((t (:foreground "grey" :background "black"))))
+   '(company-tooltip ((t (:background "black" :foreground "grey" :font "SF Mono" :height 120))))
+   '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
+   '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
+   '(company-tooltip-selection ((t (:background "grey" :foreground "#1D1D1D" :font "SF Mono" :height 120)))))
  '(eshell-prompt ((t (:inherit default))))
  '(sp-pair-overlay-face ((t nil))))
 
