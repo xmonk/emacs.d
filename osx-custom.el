6e599ede42bf96a27f372f33bf28bdc818552b0c
Juan Fuentes <jj@xmonk.org>
2017-05-25 18:38:21 -0400

    More modules

--- osx-custom.el
+++ osx-custom.el
@@ -38,22 +38,14 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (go-guru go-rename company-go go-errcheck go-eldoc color-theme-sanityinc-tomorrow company-anaconda anaconda-mode py-yapf go-mode magit flycheck browse-kill-ring whole-line-or-region codesearch expand-region web-mode markdown-mode projectile undo-tree git-timemachine xcscope paredit company-flx company use-package)))
+    (irony-eldoc flycheck-rtags irony-mode company-irony irony rtags cmake-ide ob-go cmake-mode go-guru go-rename company-go go-errcheck go-eldoc color-theme-sanityinc-tomorrow company-anaconda anaconda-mode py-yapf go-mode magit flycheck browse-kill-ring whole-line-or-region codesearch expand-region web-mode markdown-mode projectile undo-tree git-timemachine xcscope paredit company-flx company use-package)))
  '(temporary-file-directory "/tmp"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- (unless custom-enabled-themes
-   '(company-preview ((t (:background "blue" :foreground "grey"))))
-   '(company-preview-common ((t (:inherit company-preview))))
-   '(company-scrollbar-bg ((t (:background "grey" :foreground "black"))))
-   '(company-scrollbar-fg ((t (:foreground "grey" :background "black"))))
-   '(company-tooltip ((t (:background "black" :foreground "grey" :font "SF Mono" :height 120))))
-   '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
-   '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
-   '(company-tooltip-selection ((t (:background "grey" :foreground "#1D1D1D" :font "SF Mono" :height 120)))))
+ '(company-tooltip-selection ((t (:background "grey" :foreground "#1D1D1D" :font "SF Mono" :height 120))))
  '(eshell-prompt ((t (:inherit default))))
  '(sp-pair-overlay-face ((t nil))))
 
