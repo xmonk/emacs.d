333ce66122a72b0e58dae32767896c3c9aa7534b
Juan Fuentes <jj@xmonk.org>
2017-06-15 11:02:03 -0400

    fix issue with font

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
 ;; set faces
 ;; themes
 (add-to-list 'custom-theme-load-path (expand-file-name (concat user-emacs-directory "themes/")))
-(set-face-attribute 'default nil :font "SF Mono Medium" :height 120 :slant 'normal :weight 'normal)
+(set-face-attribute 'default nil :font "-*-SF Mono-semibold-normal-normal-*-12-*-*-*-m-0-iso10646-1")
 (load-theme 'jj-dark t)
 
 (defvar *site-lisp* (concat user-emacs-directory "site-lisp/")
