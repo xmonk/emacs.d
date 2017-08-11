1ad4d2614a30ccef96fd8ee49bb556984f8adb47
Juan Fuentes <jj@xmonk.org>
2017-08-10 22:28:15 -0400

    experimenting a bit with different packages

--- osx-custom.el
+++ osx-custom.el
@@ -29,9 +29,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("66ccea5e9c4b7fec8b653efe0656d67a21777ea98033c7094e1d001245d7435d" default)))
  '(flycheck-python-pyflakes-executable "pyflakes")
  '(gnutls-trustfiles
    (quote
@@ -41,14 +38,13 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (swiper no-littering flycheck-rust cargo racer rustfmt rust-mode smartscan irony-c-headers company-irony-c-headers irony irony-eldoc irony-mode company-irony ob-go cmake-mode go-guru go-rename company-go go-errcheck go-eldoc company-anaconda anaconda-mode py-yapf go-mode magit flycheck browse-kill-ring whole-line-or-region codesearch expand-region web-mode markdown-mode projectile undo-tree git-timemachine xcscope paredit company-flx company use-package)))
+    (swiper doom-themes org no-littering rustfmt irony-c-headers company-irony-c-headers irony irony-eldoc irony-mode company-irony ob-go cmake-mode go-guru go-rename company-go go-errcheck go-eldoc company-anaconda anaconda-mode py-yapf go-mode magit flycheck browse-kill-ring whole-line-or-region codesearch expand-region web-mode markdown-mode projectile undo-tree git-timemachine xcscope paredit company-flx company use-package)))
  '(temporary-file-directory "/tmp"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-tooltip-selection ((t (:background "grey" :foreground "#1D1D1D" :font "SF Mono" :height 120))))
  '(eshell-prompt ((t (:inherit default))))
  '(sp-pair-overlay-face ((t nil))))
 
