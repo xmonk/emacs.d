0e430b0e59751f36b5303209a5e9ff9a230c3c07
Juan Fuentes <jj@xmonk.org>
2017-08-13 22:12:20 -0400

    Not using swiper for now

--- site-lisp/global.el
+++ site-lisp/global.el
@@ -234,22 +234,22 @@
   (projectile-mode t))
 
 ;;; swiper
-(use-package swiper
-  :disabled
-  :diminish ivy-mode
-  :bind (("C-s" . swiper)
-	 ("C-r" . swiper)
-	 ("C-c v" . ivy-push-view)
-         ("C-c V" . ivy-pop-view))
-  :config
-  ;; (ivy-mode 1)
-  ;; (setq ivy-use-virtual-buffers t)
-  ;; (setq ivy-count-format "%d/%d ")
-  ;;advise swiper to recenter on exit
-  (defun jj/swiper-recenter (&rest args)
-    "recenter display after swiper"
-    (recenter))
-  (advice-add 'swiper :after #'jj/swiper-recenter))
+;; (use-package swiper
+;;   :disabled
+;;   :diminish ivy-mode
+;;   :bind (("C-s" . swiper)
+;; 	 ("C-r" . swiper)
+;; 	 ("C-c v" . ivy-push-view)
+;;          ("C-c V" . ivy-pop-view))
+;;   :config
+;;   ;; (ivy-mode 1)
+;;   ;; (setq ivy-use-virtual-buffers t)
+;;   ;; (setq ivy-count-format "%d/%d ")
+;;   ;;advise swiper to recenter on exit
+;;   (defun jj/swiper-recenter (&rest args)
+;;     "recenter display after swiper"
+;;     (recenter))
+;;   (advice-add 'swiper :after #'jj/swiper-recenter))
 
 (use-package dired
   :defer 10
