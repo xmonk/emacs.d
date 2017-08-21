e8506ef8c2a0b378a9c9dd2d5187053227f00055
Juan Fuentes <jj@xmonk.org>
2017-08-21 06:39:33 -0400

    Add slime base configuration

--- site-lisp/lisp-conf.el
+++ site-lisp/lisp-conf.el
@@ -28,7 +28,6 @@
   :commands lisp-mode
   :init
   (add-hook 'lisp-mode-hook 'jj/lisp-mode-hook)
-  (add-hook 'inferior-lisp-mode-hook 'jj/inferior-lisp-mode-hook)
   (add-hook 'after-save-hook 'check-parens nil t)
   :config
   (use-package info-look
@@ -43,14 +42,17 @@
     (setq-default tab-width 2)
     (setq-default indent-tabs-mode nil)
     (abbrev-mode 1)
-    (bind-key "<return>" 'newline-and-indent)
-    (hs-minor-mode)
-    (if (fboundp 'slime-mode)
-        (slime-mode t)))
+    (hs-minor-mode))
 
-  (defun jj/inferior-lisp-mode-hook()
-    (if (fboundp 'inferior-slime-mode)
-        (inferior-slime-mode t))))
+  (use-package slime
+    :commands slime
+    :init
+    ;; Replace "sbcl" with the path to your implementation
+    (setq inferior-lisp-program "sbcl"
+          slime-contribs '(slime-fancy))
+    (load (expand-file-name "~/quicklisp/slime-helper.el"))
+    :config
+    (slime-mode t)))
 
 (provide 'lisp-conf)
 
