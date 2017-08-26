d37589578b404ed78ed141859475ca23df9ab128
Juan Fuentes <jj@xmonk.org>
2017-08-26 09:44:06 -0400

    Simplified sh-mode configuration

--- site-lisp/sh-conf.el
+++ site-lisp/sh-conf.el
@@ -24,17 +24,16 @@
 ;;
 ;;; Code:
 
-(defun jj/sh-hook ()
-  "personal shell hook"
+(use-package sh-mode
+  :commands (sh-mode shell-script-mode)
+  :init
   (setq-default tab-width 4)
   (setq-default indent-tabs-mode t)
-  (setq sh-basic-offset 4
-	sh-indentation 4
-	sh-indent-for-case-label 0
-	sh-indent-for-case-alt '+))
+  (setq-default sh-basic-offset 4
+				sh-indentation 4
+				sh-indent-for-case-label 0
+				sh-indent-for-case-alt '+))
 
-(add-hook 'shell-script-mode 'jj/sh-hook)
-(add-hook 'sh-mode-hook 'jj/sh-hook)
 (provide 'sh-conf)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
