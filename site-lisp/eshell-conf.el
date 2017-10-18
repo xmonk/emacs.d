ead5d411bc9903880c3cd63606f0a6988b2258a2
Juan Fuentes <jj@xmonk.org>
2017-10-18 12:42:39 -0400

    Restructured the code to utilize use-package more efficiently

--- site-lisp/eshell-conf.el
+++ site-lisp/eshell-conf.el
@@ -24,68 +24,73 @@
 ;;
 ;;; Code:
 
-(require 'eshell nil t)
-(require 'em-smart nil t)
+(use-package eshell
+  :commands (eshell jj/esh)
+  :init
+  (add-hook 'eshell-mode-hook 'jj/eshell-mode-hook)
+  ;; (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color)
+  :config
+  (setq pcomplete-cycle-completions t)
 
-(setq eshell-where-to-jump 'begin
-      eshell-ls-use-colors nil
-      eshell-review-quick-commands nil
-      eshell-smart-space-goes-to-end t
-      eshell-history-size 500
-      eshell-save-history-on-exit t
-      eshell-cmpl-cycle-completions t
-      pcomplete-cycle-completions t
-      eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.hg\\|\\.git\\)/\\'")
+  (use-package em-ls
+	:init
+	(setq eshell-ls-use-colors nil))
 
-(defun jj/eshell-mode-hook()
-  (define-key eshell-mode-map "\C-a" 'eshell-bol)
-  (define-key eshell-mode-map "\t" 'pcomplete-list)
-  (define-key eshell-mode-map (kbd "<up>") 'previous-line)
-  (define-key eshell-mode-map (kbd "<down>") 'next-line))
+  (use-package em-smart
+	:config
+	(setq eshell-where-to-jump 'begin
+		  eshell-review-quick-commands nil
+		  eshell-smart-space-goes-to-end t))
 
-(defun jj/eshell-prompt()
-  (concat (abbreviate-file-name (eshell/pwd))
-          (if (= (user-uid) 0)
-              "# " "% ")))
 
-(defun jj/simple-eshell-prompt ()
-  (if (= (user-uid) 0)
-      "# " "% "))
+  (use-package em-hist
+	:init
+	(setq eshell-history-size 500
+		  eshell-save-history-on-exit t))
 
-(load-after esh-opt
-  (require 'em-prompt)
-  (require 'em-term)
-  (require 'em-cmpl)
 
-  ;; TODO: for some reason requiring this here breaks it, but
-  ;; requiring it after an eshell session is started works fine.
-  ;; (require 'eshell-vc)
+  (defun jj/eshell-mode-hook()
+	(define-key eshell-mode-map "\C-a" 'eshell-bol)
+	(define-key eshell-mode-map "\t" 'pcomplete-list)
+	(define-key eshell-mode-map (kbd "<up>") 'previous-line)
+	(define-key eshell-mode-map (kbd "<down>") 'next-line))
+
   (setenv "PAGER" "cat")
   (unless (string= (getenv "PROMPT_COMMAND") "")
     (setenv "PROMPT_COMMAND" ""))
-  ;; (set-face-attribute 'eshell-prompt nil :foreground "grey73")
-  (add-hook 'eshell-mode-hook 'jj/eshell-mode-hook)
 
-  (add-to-list 'eshell-visual-commands "ssh")
-  (add-to-list 'eshell-visual-commands "tail")
-  (add-to-list 'eshell-command-completions-alist
-               '("gunzip" "gz\\'"))
-  (add-to-list 'eshell-command-completions-alist
-               '("tar" "\\(\\.tar|\\.tgz\\|\\.tar\\.gz\\)\\'"))
-  (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color))
+  (use-package em-term
+	:config
+	(add-to-list 'eshell-visual-commands "ssh")
+	(add-to-list 'eshell-visual-commands "tail"))
 
-(defun eshell/cds ()
-  "Change directory to the project's root."
-  (eshell/cd (locate-dominating-file default-directory "src")))
-
-(setq eshell-prompt-regexp "^[^#%\n]*[#%] ")
+  (use-package em-cmpl
+	:config
+	(add-to-list 'eshell-command-completions-alist '("gunzip" "gz\\'"))
+	(add-to-list 'eshell-command-completions-alist '("tar" "\\(\\.tar|\\.tgz\\|\\.tar\\.gz\\)\\'"))
+	(setq eshell-cmpl-cycle-completions t)
+	(setq eshell-cmpl-dir-ignore "\\`\\(\\.\\.?\\|CVS\\|\\.svn\\|\\.hg\\|\\.git\\)/\\'"))
 
 
-;;(let ((path (split-string (getenv "PATH") ":")))
-;;  (add-to-list 'path (concat (getenv "HOME") "/bin"))
-;;  (setenv "PATH" (mapconcat 'identity path ":")))
+  (use-package em-prompt
+	:after em-dirs
+	:config
+	(setq eshell-prompt-regexp "^[^#%\n]*[#%] ")
+	(setq eshell-prompt-function  'jj/eshell-prompt))
 
-(setq eshell-prompt-function  'jj/eshell-prompt)
+  (use-package em-dirs
+	:functions eshell/pwd
+	:functions eshell/cd
+	:config
+	(defun jj/eshell-prompt()
+	  (concat (abbreviate-file-name (eshell/pwd))
+			  (if (= (user-uid) 0)
+				  "# " "% ")))
+
+	(defun jj/simple-eshell-prompt ()
+	  (if (= (user-uid) 0)
+		  "# " "% "))))
+
 
 (provide 'eshell-conf)
 
