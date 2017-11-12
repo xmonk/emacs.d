326239c2407467301aff0a040f7c6fb92db9a4dd
Juan Fuentes <jj@xmonk.org>
2017-11-11 20:41:14 -0400

    Remove old code

--- site-lisp/py-conf.el
+++ site-lisp/py-conf.el
@@ -89,17 +89,7 @@
     (split-line)
     (insert python--ipdb-breakpoint-string))
 
-  (define-key python-mode-map (kbd "<f5>") 'python-insert-breakpoint)
-
-  (defun python-set-venv (venv)
-    (interactive "GPath: ")
-    ;; set path pythonpath and exec-path if *venv* is not set in $PATH and $PYTHONPATH.
-    (defvar *venv* (expand-file-name venv))
-    (unless (and (file-directory-p *venv*) (string-match *venv* (getenv "PATH"))
-                 (string-match *venv* (getenv "PYTHONPATH")))
-      (setenv "PATH" (concat *venv* "/bin" path-separator (getenv "PATH")))
-      (setenv "PYTHONPATH" (concat (getenv "PYTHONPATH") path-separator *venv* "lib/python3.5/site-packages"))
-      (setq exec-path (append exec-path (list (concat *venv* "bin")))))))
+  (define-key python-mode-map (kbd "<f5>") 'python-insert-breakpoint))
 
 (provide 'py-conf)
 
