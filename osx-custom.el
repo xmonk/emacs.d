2676d299c84475b0c582ecdd8e3a7fe9fe4d2c33
Juan Fuentes <jj@xmonk.org>
2018-04-19 14:14:10 -0400

    Add clang-format style and fix formatting

--- osx-custom.el
+++ osx-custom.el
@@ -29,31 +29,39 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(clang-format-style
+   "{BasedOnStyle: llvm, UseTab: Always, IndentWidth: 8, AllowShortFunctionsOnASingleLine: None, KeepEmptyLinesAtTheStartOfBlocks: false}")
  '(custom-safe-themes
-   '("1f1ded5a5dfda5b9da339ae7ce49d41c470cbc34099f9bdb9342c0183ab49ff1" default))
+   (quote
+    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "1f1ded5a5dfda5b9da339ae7ce49d41c470cbc34099f9bdb9342c0183ab49ff1" default)))
  '(dired-async-mode t)
- '(ediff-window-setup-function 'ediff-setup-windows-plain)
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(flycheck-python-pyflakes-executable "pyflakes")
  '(gnutls-trustfiles
-   '("/etc/ssl/ca-bundle.pem" "/usr/local/etc/openssl/cert.pem" "~/.certs/ca-certificates.pem"))
+   (quote
+    ("/etc/ssl/ca-bundle.pem" "/usr/local/etc/openssl/cert.pem" "~/.certs/ca-certificates.pem")))
  '(grep-command "/usr/local/bin/rg")
- '(grep-find-command '("find . -type f -exec /usr/local/bin/rg \\{\\} +" . 39))
+ '(grep-find-command
+   (quote
+    ("find . -type f -exec /usr/local/bin/rg \\{\\} +" . 39)))
  '(grep-find-template
    "find <D> <X> -type f <F> -exec /usr/local/bin/rg -e <R> \\{\\} +")
- '(grep-highlight-matches 'always)
+ '(grep-highlight-matches (quote always))
  '(grep-program (executable-find "rg") t)
  '(indent-tabs-mode t)
  '(magit-item-highlight-face nil)
  '(package-enable-at-startup t)
  '(package-quickstart t)
  '(package-selected-packages
-   '(swiper diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree smartscan py-yapf projectile paredit ob-go markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring))
+   (quote
+    (color-theme-sanityinc-tomorrow-colors clang-format color-theme-sanityinc-tomorrow swiper diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree smartscan py-yapf projectile paredit ob-go markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring)))
  '(projectile-globally-ignored-directories
-   '(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc"))
- '(projectile-globally-ignored-files '("TAGS" ".DS_Store" ".elc" ".pyc"))
- '(slime-company-completion 'fuzzy)
+   (quote
+    (".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc")))
+ '(projectile-globally-ignored-files (quote ("TAGS" ".DS_Store" ".elc" ".pyc")))
+ '(slime-company-completion (quote fuzzy))
  '(temporary-file-directory "/tmp")
- '(tramp-syntax 'default nil (tramp)))
+ '(tramp-syntax (quote default) nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
