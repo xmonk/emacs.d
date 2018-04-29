f9137950d6de82ced9954e7f4fece9e12db57fbb
Juan Fuentes <jj@xmonk.org>
2018-04-29 15:33:06 -0400

    formatting.

--- osx-custom.el
+++ osx-custom.el
@@ -36,11 +36,11 @@
  '(flycheck-python-pyflakes-executable "pyflakes")
  '(gnutls-trustfiles
    (quote
-	("/etc/ssl/ca-bundle.pem" "/usr/local/etc/openssl/cert.pem" "~/.certs/ca-certificates.pem")))
+    ("/etc/ssl/ca-bundle.pem" "/usr/local/etc/openssl/cert.pem" "~/.certs/ca-certificates.pem")))
  '(grep-command "/usr/local/bin/rg")
  '(grep-find-command
    (quote
-	("find . -type f -exec /usr/local/bin/rg \\{\\} +" . 39)))
+    ("find . -type f -exec /usr/local/bin/rg \\{\\} +" . 39)))
  '(grep-find-template
    "find <D> <X> -type f <F> -exec /usr/local/bin/rg -e <R> \\{\\} +")
  '(grep-highlight-matches (quote always))
@@ -51,10 +51,10 @@
  '(package-quickstart t)
  '(package-selected-packages
    (quote
-	(clang-format swiper diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree smartscan py-yapf projectile paredit ob-go markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring)))
+    (clang-format swiper diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree smartscan py-yapf projectile paredit ob-go markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring)))
  '(projectile-globally-ignored-directories
    (quote
-	(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc")))
+    (".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc")))
  '(projectile-globally-ignored-files (quote ("TAGS" ".DS_Store" ".elc" ".pyc")))
  '(slime-company-completion (quote fuzzy))
  '(temporary-file-directory "/tmp")
