0a6dbf8a1949ecd43832495231bd454f21a9434a
Juan Fuentes <jj@xmonk.org>
2018-04-29 21:15:53 -0400

    More use-package tweaks

--- site-lisp/eshell-conf.el
+++ site-lisp/eshell-conf.el
@@ -33,20 +33,23 @@
   (setq pcomplete-cycle-completions t)
 
   (use-package em-ls
+    :after eshell
     :init
     (setq eshell-ls-use-colors nil))
 
   (use-package em-smart
-    :config
+    :after eshell
+    :init
     (setq eshell-where-to-jump 'begin
-		  eshell-review-quick-commands nil
-		  eshell-smart-space-goes-to-end t))
+	  eshell-review-quick-commands nil
+	  eshell-smart-space-goes-to-end t))
 
 
   (use-package em-hist
+    :after eshell
     :init
     (setq eshell-history-size 500
-		  eshell-save-history-on-exit t))
+	  eshell-save-history-on-exit t))
 
 
   (defun jj/eshell-mode-hook()
@@ -60,11 +63,13 @@
     (setenv "PROMPT_COMMAND" ""))
 
   (use-package em-term
+    :after eshell
     :config
     (add-to-list 'eshell-visual-commands "ssh")
     (add-to-list 'eshell-visual-commands "tail"))
 
   (use-package em-cmpl
+    :after eshell
     :config
     (add-to-list 'eshell-command-completions-alist '("gunzip" "gz\\'"))
     (add-to-list 'eshell-command-completions-alist '("tar" "\\(\\.tar|\\.tgz\\|\\.tar\\.gz\\)\\'"))
@@ -79,17 +84,18 @@
     (setq eshell-prompt-function  'jj/eshell-prompt))
 
   (use-package em-dirs
+    :after eshell
     :functions eshell/pwd
     :functions eshell/cd
     :config
     (defun jj/eshell-prompt()
       (concat (abbreviate-file-name (eshell/pwd))
-			  (if (= (user-uid) 0)
-				  "# " "% ")))
+	      (if (= (user-uid) 0)
+		  "# " "% ")))
 
     (defun jj/simple-eshell-prompt ()
       (if (= (user-uid) 0)
-		  "# " "% "))))
+	  "# " "% "))))
 
 
 (provide 'eshell-conf)
