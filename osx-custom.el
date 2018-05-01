b37a2a95cee6499a43bb155d2cb093656d3ec53b
Juan Fuentes <jj@xmonk.org>
2018-04-30 20:04:54 -0400

    New packages.

--- osx-custom.el
+++ osx-custom.el
@@ -32,33 +32,28 @@
  '(clang-format-style
    "{BasedOnStyle: llvm, UseTab: Always, IndentWidth: 8, AllowShortFunctionsOnASingleLine: None, KeepEmptyLinesAtTheStartOfBlocks: false}")
  '(dired-async-mode t)
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(flycheck-python-pyflakes-executable "pyflakes")
  '(gnutls-trustfiles
-   (quote
-    ("/etc/ssl/ca-bundle.pem" "/usr/local/etc/openssl/cert.pem" "~/.certs/ca-certificates.pem")))
+   '("/etc/ssl/ca-bundle.pem" "/usr/local/etc/openssl/cert.pem" "~/.certs/ca-certificates.pem"))
  '(grep-command "/usr/local/bin/rg")
- '(grep-find-command
-   (quote
-    ("find . -type f -exec /usr/local/bin/rg \\{\\} +" . 39)))
+ '(grep-find-command '("find . -type f -exec /usr/local/bin/rg \\{\\} +" . 39))
  '(grep-find-template
    "find <D> <X> -type f <F> -exec /usr/local/bin/rg -e <R> \\{\\} +")
- '(grep-highlight-matches (quote always))
+ '(grep-highlight-matches 'always)
  '(grep-program (executable-find "rg") t)
  '(indent-tabs-mode t)
  '(magit-item-highlight-face nil)
- '(package-enable-at-startup nil)
+ '(package-enable-at-startup t)
  '(package-quickstart t)
  '(package-selected-packages
-   (quote
-    (flycheck-rust clang-format swiper diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree smartscan py-yapf projectile paredit markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring)))
+   '(markdown-preview-mode edit-indirect flycheck-rust clang-format swiper diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree smartscan py-yapf projectile paredit markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring))
  '(projectile-globally-ignored-directories
-   (quote
-    (".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc")))
- '(projectile-globally-ignored-files (quote ("TAGS" ".DS_Store" ".elc" ".pyc")))
- '(slime-company-completion (quote fuzzy))
+   '(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc"))
+ '(projectile-globally-ignored-files '("TAGS" ".DS_Store" ".elc" ".pyc"))
+ '(slime-company-completion 'fuzzy)
  '(temporary-file-directory "/tmp")
- '(tramp-syntax (quote default) nil (tramp)))
+ '(tramp-syntax 'default nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
