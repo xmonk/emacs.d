8b8a19b91088cabee5aece224b5c7f48a94563ae
Juan Fuentes <jj@xmonk.org>
2018-05-01 22:15:34 -0400

    New packages and trying out some use-package options.

--- osx-custom.el
+++ osx-custom.el
@@ -47,13 +47,15 @@
  '(package-enable-at-startup t)
  '(package-quickstart t)
  '(package-selected-packages
-   '(markdown-preview-mode edit-indirect flycheck-rust clang-format swiper diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree smartscan py-yapf projectile paredit markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring))
+   '(edit-indirect flycheck-rust clang-format swiper diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree py-yapf projectile paredit markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring))
  '(projectile-globally-ignored-directories
    '(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc"))
  '(projectile-globally-ignored-files '("TAGS" ".DS_Store" ".elc" ".pyc"))
  '(slime-company-completion 'fuzzy)
  '(temporary-file-directory "/tmp")
- '(tramp-syntax 'default nil (tramp)))
+ '(tramp-syntax 'default nil (tramp))
+ '(use-package-compute-statistics nil)
+ '(use-package-expand-minimally t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
