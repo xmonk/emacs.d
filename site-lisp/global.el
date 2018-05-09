16733d9e782952bde00bdaf55e4bf467d2d89ae0
Juan Fuentes <jj@xmonk.org>
2018-05-09 10:16:59 -0400

    Trying out helpful for a bit.

--- site-lisp/global.el
+++ site-lisp/global.el
@@ -140,6 +140,15 @@
 (defvar tramp-default-method "ssh")
 (defvar tramp-ssh-controlmaster-options nil)
 
+;;; helpful
+(use-package helpful
+  :ensure t
+  :bind (("C-h f" . helpful-callable)
+         ("C-h v" . helpful-variable)
+         ("C-h k" . helpful-key)
+         ("C-c C-d" . helpful-at-point)
+         ("C-h F" . helpful-function)
+         ("C-h C" . helpful-command)))
 ;;; ibuffer
 (use-package ibuffer
   :commands ibuffer)
