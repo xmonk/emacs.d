2807696444132cecd3e9136cb97a897d65352c63
Juan Fuentes <jj@xmonk.org>
2018-05-10 10:23:27 -0400

    removed custom options that are now being loaded by use-package

--- osx-custom.el
+++ osx-custom.el
@@ -29,13 +29,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(clang-format-style
-   "{BasedOnStyle: llvm, UseTab: Always, IndentWidth: 8, AllowShortFunctionsOnASingleLine: None, KeepEmptyLinesAtTheStartOfBlocks: false}")
- '(dired-async-mode t)
+  '(dired-async-mode t)
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(flycheck-python-pyflakes-executable "pyflakes")
- '(gdb-many-windows t)
- '(gdb-show-main t)
  '(gnutls-trustfiles
    '("/etc/ssl/ca-bundle.pem" "/usr/local/etc/openssl/cert.pem" "~/.certs/ca-certificates.pem"))
  '(grep-command "/usr/local/bin/rg")
@@ -45,12 +41,8 @@
  '(grep-highlight-matches 'always)
  '(grep-program (executable-find "rg") t)
  '(indent-tabs-mode t)
- '(magit-item-highlight-face nil)
  '(package-selected-packages
    '(magithub pipenv flycheck-irony cmake-mode edit-indirect clang-format diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree py-yapf projectile paredit markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring))
- '(projectile-globally-ignored-directories
-   '(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc"))
- '(projectile-globally-ignored-files '("TAGS" ".DS_Store" ".elc" ".pyc"))
  '(slime-company-completion 'fuzzy)
  '(temporary-file-directory "/tmp")
  '(tramp-syntax 'default nil (tramp))
@@ -60,8 +52,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(eshell-prompt ((t (:inherit default))))
- '(sp-pair-overlay-face ((t nil))))
+  '(sp-pair-overlay-face ((t nil))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; osx-custom.el ends here
