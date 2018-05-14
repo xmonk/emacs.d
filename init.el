71680c682c060e8477f9ecdd85c80980fe48251a
Juan Fuentes <jj@xmonk.org>
2018-05-13 20:24:49 -0400

    trying out tomorrow themes.

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
                              (setq gc-cons-threshold 800000)))
 ;;; themes
 (add-to-list 'custom-theme-load-path (expand-file-name (concat user-emacs-directory "themes/")))
-(load-theme 'jj-darkcl t)
+(load-theme #'sanityinc-tomorrow-night t)
 
 ;;; frame
 (unless window-system
