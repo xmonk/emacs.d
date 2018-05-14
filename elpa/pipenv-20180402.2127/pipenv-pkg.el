f2070225dbadf8a283aa6711efb1cc3b054b4620
Juan Fuentes <jj@xmonk.org>
2018-05-13 20:24:04 -0400

    Add elpa to repo to make moving over to new machines simpler.

---
+++ elpa/pipenv-20180402.2127/pipenv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pipenv" "20180402.2127" "A Pipenv porcelain." '((emacs "25.1") (f "0.19.0") (s "1.12.0")) :commit "8298daefee1b044c58390c861a6a70937cb0601b" :url "https://github.com/pwalsh/pipenv.el")
