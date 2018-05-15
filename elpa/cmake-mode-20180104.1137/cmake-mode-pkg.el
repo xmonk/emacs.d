c703745d83cc210f835b3705e42b93eb494ad5b5
Juan Fuentes <jj@xmonk.org>
2018-05-14 21:53:12 -0400

    Package updates..

--- elpa/cmake-mode-20180104.1137/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20180104.1137" "major-mode for editing CMake sources" 'nil :commit "7489d9cb241f199471499e0952a13ea48fec1f9e")
--- elpa/magit-20180514.546/magit-pkg.el
+++ elpa/magit-20180514.546/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20180512.1350" "A Git porcelain inside Emacs."
+(define-package "magit" "20180514.546" "A Git porcelain inside Emacs."
   '((emacs "24.4")
     (async "20170823")
     (dash "20170810")
