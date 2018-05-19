59f9d053d24c41c2012c702e28b0647077e54fa8
Juan Fuentes <jj@xmonk.org>
2018-05-18 20:39:46 -0400

    update packages.

--- osx-custom.el
+++ osx-custom.el
@@ -46,7 +46,7 @@
  '(package-enable-at-startup t)
  '(package-quickstart t)
  '(package-selected-packages
-   '(pipenv exec-path-from-shell evil whole-line-or-region yaml-mode color-theme-sanityinc-tomorrow magithub flycheck-irony cmake-mode edit-indirect clang-format diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree py-yapf projectile paredit markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring))
+   '(pipenv exec-path-from-shell whole-line-or-region yaml-mode color-theme-sanityinc-tomorrow magithub flycheck-irony cmake-mode edit-indirect clang-format diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree py-yapf projectile paredit markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flx-ido expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch browse-kill-ring))
  '(projectile-globally-ignored-directories
    '(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc"))
  '(projectile-globally-ignored-files '("TAGS" ".DS_Store" ".elc" ".pyc"))
