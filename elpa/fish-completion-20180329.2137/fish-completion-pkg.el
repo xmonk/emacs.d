d776dd90a4b886c35984ef559505b41dc9c2d68c
Juan Fuentes <jj@xmonk.org>
2018-05-21 22:12:14 -0400

    Trying out fish-mode and fish-completion

---
+++ elpa/fish-completion-20180329.2137/fish-completion-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fish-completion" "20180329.2137" "Add fish completion to pcomplete (shell and Eshell)" 'nil :commit "3e3ed1f19fa778b7c35ad88e033dce5a6b1fc153" :url "https://github.com/Ambrevar/emacs-fish-completion")
