d4f525a944a630f7e85e275cf21791cf408472dd
Juan Fuentes <jj@xmonk.org>
2018-05-27 16:49:35 -0400

    Trying out the early-init.el thing, seems to be working better now.

---
+++ early-init.el
@@ -0,0 +1,3 @@
+(setq package-enable-at-startup t)
+(setq package-quickstart t)
+(package-initialize)
--- osx-custom.el
+++ osx-custom.el
@@ -48,8 +48,6 @@
  '(grep-highlight-matches 'always)
  '(grep-program (executable-find "rg") t)
  '(indent-tabs-mode t)
- '(package-enable-at-startup t)
- '(package-quickstart t)
  '(package-selected-packages
    '(ace-window evil exec-path-from-shell doom-themes pipenv whole-line-or-region yaml-mode magithub flycheck-irony cmake-mode edit-indirect clang-format diminish org elisp-slime-nav irony anaconda-mode go-mode flycheck company xcscope web-mode use-package undo-tree py-yapf projectile paredit markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine expand-region company-irony-c-headers company-irony company-go company-flx company-anaconda browse-kill-ring))
  '(projectile-globally-ignored-directories
