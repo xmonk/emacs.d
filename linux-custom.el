7a52536c46eb186bdb0c57670c27c842a595756e
Juan Fuentes <jj@xmonk.org>
2018-05-28 08:24:59 -0400

    Add a few new config options

--- linux-custom.el
+++ linux-custom.el
@@ -28,11 +28,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(clang-format-style
-   "{BasedOnStyle: llvm, UseTab: Always, IndentWidth: 8, AllowShortFunctionsOnASingleLine: None, KeepEmptyLinesAtTheStartOfBlocks: false}" t)
  '(dired-async-mode t)
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(flycheck-python-pyflakes-executable "pyflakes")
+ '(gdb-many-windows t t)
+ '(gdb-show-main t t)
  '(grep-command "/usr/bin/rg")
  '(grep-find-command '("find . -type f -exec /usr/bin/rg \\{\\} +" . 39))
  '(grep-find-template
@@ -41,18 +41,17 @@
  '(grep-program (executable-find "rg") t)
  '(indent-tabs-mode t)
  '(magit-item-highlight-face nil)
- '(package-enable-at-startup t)
- '(package-quickstart t)
- '(use-package-compute-statistics t)
- '(use-package-verbose t)
  '(package-selected-packages
-   '(ace-window auto-virtualenvwrapper smex fish-mode pylint fish-completion doom-themes lua-mode whole-line-or-region yaml-mode edit-indirect xcscope web-mode use-package undo-tree swiper smartscan py-yapf projectile paredit org ob-go markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flycheck flx-ido expand-region elisp-slime-nav diminish company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch clang-format browse-kill-ring))
+   '(ace-window auto-virtualenvwrapper pylint doom-themes lua-mode whole-line-or-region yaml-mode edit-indirect xcscope web-mode use-package undo-tree swiper smartscan py-yapf projectile paredit org ob-go markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flycheck flx-ido expand-region elisp-slime-nav diminish company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch clang-format browse-kill-ring))
  '(projectile-globally-ignored-directories
    '(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc"))
  '(projectile-globally-ignored-files '("TAGS" ".DS_Store" ".elc" ".pyc"))
  '(slime-company-completion 'fuzzy)
  '(temporary-file-directory "/tmp")
- '(tramp-syntax 'default nil (tramp)))
+ '(tramp-syntax 'default nil (tramp))
+ '(use-package-compute-statistics nil)
+ '(use-package-verbose t)
+ '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
