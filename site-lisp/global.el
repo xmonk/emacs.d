c417fc23ed01ab577ba53ce6dd3517ae98c4fd55
Juan Fuentes <jj@xmonk.org>
2018-05-28 08:17:39 -0400

    Moved markdown config to it's own file.

--- site-lisp/global.el
+++ site-lisp/global.el
@@ -148,28 +148,6 @@
 (use-package doc-view
   :commands doc-view)
 
-;;; Markdown
-(use-package markdown-mode
-  :ensure t
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init
-  (setq markdown-command "multimarkdown"
-        markdown-fontify-code-blocks-natively t
-        markdown-enable-math t
-        markdown-header-scaling t
-        markdown-hide-urls t
-        markdown-marginalize-headers t
-        markdown-marginalize-headers-margin-width 4
-        markdown-fontify-code-blocks-natively t)
-  (add-hook 'markdown-mode-hook #'flyspell-mode))
-
-(use-package edit-indirect
-  :ensure t
-  :defer)
-
 ;;; white space mode
 (use-package whitespace
   :init
