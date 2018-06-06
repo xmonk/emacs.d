8c5530d5ec2d7ebad95159b3acd188e9ea85e1f6
Juan Fuentes <jj@xmonk.org>
2018-06-05 20:55:03 -0400

    Version bump for: ace-window, anaconda-mode, company-anaconda, org, magit, doom-themes

---
+++ elpa/ace-window-20180601.950/ace-window-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ace-window" "20180601.950" "Quickly switch windows." '((avy "0.2.0")) :commit "c35618ed00b57b4072697d49070e9ade7771fc37" :keywords '("window" "location") :url "https://github.com/abo-abo/ace-window")
--- elpa/anaconda-mode-20180603.703/anaconda-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "anaconda-mode" "20180603.703" "Code navigation, documentation lookup and completion for Python" '((emacs "25") (pythonic "0.1.0") (dash "2.6.0") (s "1.9") (f "0.16.2")) :commit "c593e0804d96ceca865ecb7eaf6ce4ec7ad89cfd" :url "https://github.com/proofit404/anaconda-mode")
