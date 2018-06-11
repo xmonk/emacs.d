8648c564c00cac800415a09158099093d07bfdc1
Juan Fuentes <jj@xmonk.org>
2018-06-11 07:56:07 -0400

    Trying out lsp-python.

---
+++ site-lisp/lsp-conf.el
@@ -0,0 +1,34 @@
+;; lsp-mode configuration..
+
+(use-package lsp-mode
+  :ensure t
+  :init
+  (add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
+  (add-hook 'python-mode-hook (lambda () (lsp-python-enable)))
+  :config
+  (use-package lsp-imenu
+    :after lsp-mode)
+  (lsp-define-stdio-client lsp-python "python"
+                           #'jj/pwd
+                           ;; #'projectile-project-root
+                           '("pyls")))
+
+(use-package lsp-ui
+  :ensure t
+  :after lsp-mode
+  :init
+  (setq lsp-ui-sideline-ignore-duplicate t)
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package company-lsp
+  :ensure t
+  :after lsp-mode
+  :init
+  (add-hook 'company-mode-hook (lambda () (add-to-list 'company-backends 'company-lsp)))
+  (add-hook 'lsp-after-initialize-hook 'lsp-set-cfg)
+  (defun lsp-set-cfg ()
+    (let ((lsp-cfg `(:pyls (:configurationSources ("flake8")))))
+      ;; TODO: check lsp--cur-workspace here to decide per server / project
+      (lsp--set-configuration lsp-cfg))))
+
+(provide 'lsp-conf)
--- site-lisp/py-conf.el
+++ site-lisp/py-conf.el
@@ -52,6 +52,7 @@
     (add-hook 'python-mode-hook 'py-yapf-enable-on-save))
 
   (use-package anaconda-mode
+    :disabled
     :ensure t
     :after python
     :init
@@ -59,6 +60,7 @@
     (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
 
   (use-package company-anaconda
+    :disabled
     :ensure t
     :after (anaconda-mode company)
     :init
