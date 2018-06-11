e5e059f6d2d01768aac05fb4d361c3a14992764e
Juan Fuentes <jj@xmonk.org>
2018-06-11 07:49:31 -0400

    Move window setup to early init see if it works..

--- early-init.el
+++ early-init.el
@@ -1,3 +1,11 @@
+(setq inhibit-startup-message t)
 (setq package-enable-at-startup t)
 (setq package-quickstart t)
 (package-initialize)
+
+(when (window-system)
+  ;; set font
+  (if (= 5760 (display-pixel-width))
+      (set-face-attribute 'default nil :font "PragmataPro" :height (floor (* 10 9.5)))
+    (set-face-attribute 'default nil :font "PragmataPro" :height (floor (* 10 11.5))))
+  (setenv "RIPGREP_CONFIG_PATH" (concat (getenv "HOME") "/" ".ripgreprc")))
