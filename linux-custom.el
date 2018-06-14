d77ebe4c26b7974c5623c9cfc638b9a68d4ad0fa
Juan Fuentes <jj@xmonk.org>
2018-06-14 07:59:45 -0400

    package updates..

--- linux-custom.el
+++ linux-custom.el
@@ -28,6 +28,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-term-color-vector
+   [unspecified "#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#c5c8c6"] t)
  '(dired-async-mode t)
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(flycheck-flake8rc "/home/jfuentes/.config/flake8")
