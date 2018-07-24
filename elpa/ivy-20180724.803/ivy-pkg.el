21e40d8df8b1a36079b79eaf38dfbeb5358e64e5
Juan Fuentes <jj@xmonk.org>
2018-07-24 17:08:55 -0400

    Installed ivy-xref and update ivy

--- elpa/ivy-20180724.803/ivy-pkg.el
+++ elpa/ivy-20180724.803/ivy-pkg.el
@@ -1,4 +1,4 @@
-(define-package "ivy" "20180723.846" "Incremental Vertical completYon"
+(define-package "ivy" "20180724.803" "Incremental Vertical completYon"
   '((emacs "24.1"))
   :keywords
   '("matching")
