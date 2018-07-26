8f9e2ee628f2cdd216f0a4407e51aaf14069af4a
Juan Fuentes <jj@xmonk.org>
2018-07-26 18:46:24 -0400

    setup persistent undo.

--- site-lisp/utils.el
+++ site-lisp/utils.el
@@ -36,7 +36,11 @@
   (undo-tree-mode t)
   (global-undo-tree-mode t)
   (setq undo-tree-visualizer-relative-timestamps t)
-  (setq undo-tree-visualizer-timestamps t))
+  (setq undo-tree-visualizer-timestamps t)
+  (setq undo-tree-auto-save-history t)
+  (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
+  (unless (file-directory-p "~/.emacs.d/undo")
+    (make-directory "~/.emacs.d/undo")))
 
 ;;; counsel
 (use-package counsel
