01d0b401805b0de2206618417e4fd94215876032
Juan Fuentes <jj@xmonk.org>
2018-07-26 18:42:54 -0400

    Updated packages

---
+++ elpa/counsel-20180726.825/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20180726.825" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "bfa6e588e88d25b1ac354e6bfc9a5c87cb938cfc" :keywords '("convenience" "matching" "tools") :url "https://github.com/abo-abo/swiper")
