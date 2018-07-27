f91f7638748370aa120a90491f524965dc91d405
Juan Fuentes <jj@xmonk.org>
2018-07-27 11:20:42 -0400

    customizations..

--- linux-custom.el
+++ linux-custom.el
@@ -28,13 +28,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#011627" "#EF5350" "#ADDB67" "#FFEB95" "#82AAFF" "#C792EA" "#7FDBCA" "#D6DEEB"])
  '(ansi-term-color-vector
    [unspecified "#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#c5c8c6"] t)
  '(clang-format-style
    "{BasedOnStyle: llvm, UseTab: Always, IndentWidth: 8, AllowShortFunctionsOnASingleLine: None, KeepEmptyLinesAtTheStartOfBlocks: false}" t)
  '(column-number-mode t)
+ '(compilation-message-face 'default)
  '(dired-async-mode t)
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
+ '(enable-recursive-minibuffers t)
+ '(fci-rule-color "#010F1D")
  '(flycheck-flake8rc "/home/jfuentes/.config/flake8")
  '(flycheck-python-flake8-executable "/home/jfuentes/.local/bin/flake8")
  '(flycheck-python-pycompile-executable "python3")
@@ -47,7 +52,25 @@
    "find <D> <X> -type f <F> -exec /usr/bin/rg -e <R> \\{\\} +")
  '(grep-highlight-matches 'always)
  '(grep-program (executable-find "rg") t)
+ '(highlight-changes-colors '("#EF5350" "#7E57C2"))
+ '(highlight-tail-colors
+   '(("#010F1D" . 0)
+     ("#B44322" . 20)
+     ("#34A18C" . 30)
+     ("#3172FC" . 50)
+     ("#B49C34" . 60)
+     ("#B44322" . 70)
+     ("#8C46BC" . 85)
+     ("#010F1D" . 100)))
  '(indent-tabs-mode t)
+ '(ivy-count-format "(%d/%d) ")
+ '(ivy-display-style 'fancy)
+ '(ivy-use-virtual-buffers t)
+ '(ivy-virtual-abbreviate 'full)
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d0d0d" "#41728e"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d0d0d" "#b5bd68"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d0d0d" "#5a5b5a"))
+ '(magit-diff-use-overlays nil)
  '(magit-item-highlight-face nil)
  '(menu-bar-mode t)
  '(network-security-level 'high)
@@ -55,6 +78,8 @@
    '(ivy-xref ivy-rich deadgrep auto-virtualenvwrapper counsel counsel-gtags counsel-projectile counsel-pydoc night-owl-theme ggtags restclient spinner lsp-imenu company-lsp lsp-ui lsp-mode
               (lsp-mode lsp-ui company-lsp)
               exec-path-from-shell eybrowse ace-window pylint doom-themes lua-mode whole-line-or-region yaml-mode edit-indirect xcscope web-mode use-package undo-tree swiper smartscan py-yapf projectile paredit org ob-go markdown-mode magit irony-eldoc ibuffer-vc go-rename go-guru go-errcheck go-eldoc git-timemachine flycheck flx-ido expand-region elisp-slime-nav diminish company-irony-c-headers company-irony company-go company-flx company-anaconda codesearch clang-format browse-kill-ring))
+ '(pos-tip-background-color "#FFF9DC")
+ '(pos-tip-foreground-color "#011627")
  '(projectile-globally-ignored-directories
    '(".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "elpa" "venv" ".egg*" "__pycache__" "var" "etc"))
  '(projectile-globally-ignored-files '("TAGS" ".DS_Store" ".elc" ".pyc"))
@@ -66,7 +91,31 @@
  '(tramp-syntax 'default nil (tramp))
  '(use-package-compute-statistics nil)
  '(use-package-verbose t)
- '(vc-follow-symlinks t))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#C792EA")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#FFEB95")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#F78C6C")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#7FDBCA")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#82AAFF")))
+ '(vc-annotate-very-old-color nil)
+ '(vc-follow-symlinks t)
+ '(weechat-color-list
+   '(unspecified "#011627" "#010F1D" "#DC2E29" "#EF5350" "#D76443" "#F78C6C" "#D8C15E" "#FFEB95" "#5B8FFF" "#82AAFF" "#AB69D7" "#C792EA" "#AFEFE2" "#7FDBCA" "#D6DEEB" "#FFFFFF"))
+ '(xterm-mouse-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
