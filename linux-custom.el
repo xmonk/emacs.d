1b7bf97aab620e35e6ba058dd6e14145aaa47a48
Juan Fuentes <jj@xmonk.org>
2018-07-30 08:55:47 -0400

    Looking for a middle ground in font size between native and external displays

--- linux-custom.el
+++ linux-custom.el
@@ -121,7 +121,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 90 :width normal))))
+ '(default ((t (:family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 100 :width normal))))
  '(eshell-prompt ((t (:inherit default))))
  '(sp-pair-overlay-face ((t nil))))
 
