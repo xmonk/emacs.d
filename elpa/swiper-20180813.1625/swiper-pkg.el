54c0aaedd0b7fb5da529dc16087fc2d1283e19c1
Juan Fuentes <jj@xmonk.org>
2018-08-15 08:05:55 -0400

    Update packages.

---
+++ elpa/swiper-20180813.1625/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20180813.1625" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "2f5576ae9bfa7167a697dc34df05ca621d32b344" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
