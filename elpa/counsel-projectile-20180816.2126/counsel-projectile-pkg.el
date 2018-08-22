e843e4ce8fe9b697a5df55a9737d114be1f034db
Juan Fuentes <jj@xmonk.org>
2018-08-22 14:49:11 -0400

    update packages

--- elpa/counsel-projectile-20180816.2126/counsel-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel-projectile" "20180816.2126" "Ivy integration for Projectile" '((counsel "0.10.0") (projectile "1.0.0")) :commit "1e015c5378de5add512f833f30d3f3d11e0c77ed" :keywords '("project" "convenience") :authors '(("Eric Danan")) :maintainer '("Eric Danan") :url "https://github.com/ericdanan/counsel-projectile")
