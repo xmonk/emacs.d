1e0ff951e2e2f44dbed600908791bd562de589e8
Juan Fuentes <jj@xmonk.org>
2018-08-29 20:52:38 -0400

    package updates

--- elpa/fish-completion-20180616.1923/fish-completion-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "fish-completion" "20180616.1923" "Add fish completion to pcomplete (shell and Eshell)" 'nil :commit "bac15fda1392a891070574dfe5d2d50b10831e8b" :authors '(("Pierre Neidhardt" . "ambrevar@gmail.com")) :maintainer '("Pierre Neidhardt" . "ambrevar@gmail.com") :url "https://gitlab.com/Ambrevar/emacs-fish-completion")
