f7ec713f32d6a96298de8cef2fa357cff0ffe12b
Juan Fuentes <jj@xmonk.org>
2018-09-04 08:32:55 -0400

    update magit, counsel-projectile, ob-restclient

---
+++ elpa/counsel-projectile-20180903.1234/counsel-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-projectile" "20180903.1234" "Ivy integration for Projectile" '((counsel "0.10.0") (projectile "1.0.0")) :commit "175b4a787406ebd53e62186f1d3b46b1aed4324f" :keywords '("project" "convenience") :authors '(("Eric Danan")) :maintainer '("Eric Danan") :url "https://github.com/ericdanan/counsel-projectile")
