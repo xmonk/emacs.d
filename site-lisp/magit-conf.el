8045d093b68faa2c2e78d77c4bcf4560447c9289
Juan Fuentes <jj@xmonk.org>
2018-09-04 08:41:27 -0400

    cleanup magit config and enabled magit-todos

--- site-lisp/magit-conf.el
+++ site-lisp/magit-conf.el
@@ -30,12 +30,6 @@
   :init
   (defalias 'mgit 'magit-status)
   :config
-  (use-package magithub
-    :disabled
-    :ensure t
-    :after magit
-    :config
-    (magithub-feature-autoinject t))
   (defadvice magit-status (around magit-fullscreen activate)
     (window-configuration-to-register :magit-fullscreen)
     ad-do-it
@@ -43,7 +37,19 @@
 
   (defadvice magit-mode-quit-window (after magit-restore-screen activate)
     (jump-to-register :magit-fullscreen)))
+(use-package magithub
+    :disabled
+    :ensure t
+    :after magit
+    :config
+    (magithub-feature-autoinject t))
 
+(use-package magit-todos
+  :ensure t
+  :after magit
+  :commands magit-todos-mode
+  :init
+  (magit-todos-mode 1))
 
 (provide 'magit-conf)
 
