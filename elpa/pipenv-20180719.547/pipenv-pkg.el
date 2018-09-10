e95c549ef335a245b0925d4df53bc096b360f9f5
Juan Fuentes <jj@xmonk.org>
2018-09-10 09:15:45 -0400

    update packages

--- elpa/pipenv-20180719.547/pipenv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pipenv" "20180719.547" "A Pipenv porcelain." '((emacs "25.1") (f "0.19.0") (s "1.12.0")) :commit "5582bf60577de74e6301871c6b77ac86b6ce1970" :authors '(("Paul Walsh" . "paulywalsh@gmail.com")) :maintainer '("Paul Walsh" . "paulywalsh@gmail.com") :url "https://github.com/pwalsh/pipenv.el")
