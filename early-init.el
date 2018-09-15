b6199a5ad864cd63a3ddc2b1cbbec963463730e5
Juan Fuentes <jj@xmonk.org>
2018-09-14 22:12:27 -0400

    reorganize

--- early-init.el
+++ early-init.el
@@ -1,8 +1,8 @@
-(face-spec-set 'default
-               '((((type x)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 100 :width normal)
-                 (((type ns)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 135 :width normal)
-                 (t :family :height 100)))
 (setq inhibit-startup-message t)
 (setq package-enable-at-startup t)
 (setq package-quickstart t)
 (package-initialize)
+(face-spec-set 'default
+               '((((type x)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 100 :width normal)
+                 (((type ns)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 135 :width normal)
+                 (t :family :height 100)))
