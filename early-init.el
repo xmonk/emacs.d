f9c78a2e493719d27b8a29645de87416fa425989
Juan Fuentes <jj@xmonk.org>
2018-09-15 16:15:31 -0400

    Move back font faces to custom.el

--- early-init.el
+++ early-init.el
@@ -1,8 +1,4 @@
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
-(face-spec-set 'default
-               '((((type x)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 135 :width normal)
-                 (((type ns)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 135 :width normal)
-                 (t :family :height 140)))
 (setq package-enable-at-startup t)
 (setq package-quickstart t)
 (package-initialize)
--- osx-custom.el
+++ osx-custom.el
@@ -114,6 +114,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 135 :width normal))))
  '(eshell-prompt ((t (:inherit default))))
  '(sp-pair-overlay-face ((t nil))))
 
