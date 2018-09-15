53c3be9f3c2c82f65b4fd8fcec4ccf26f35b94e5
Juan Fuentes <jj@xmonk.org>
2018-09-15 15:03:48 -0400

    Move font definition back to custom.el

--- early-init.el
+++ early-init.el
@@ -2,7 +2,3 @@
 (setq package-enable-at-startup t)
 (setq package-quickstart t)
 (package-initialize)
-(face-spec-set 'default
-               '((((type x)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 100 :width normal)
-                 (((type ns)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 135 :width normal)
-                 (t :family :height 100)))
