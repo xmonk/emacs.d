ffa0d583de9c939902c9a6681c0a98121bd5dc1c
Juan Fuentes <jj@xmonk.org>
2018-09-16 21:00:25 -0400

    Move font and frame setup to early-init.el

--- early-init.el
+++ early-init.el
@@ -1,3 +1,11 @@
+(face-spec-set 'default
+               '((((type x)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 100 :width normal)
+                 (((type ns)) :family "PragmataPro Mono" :foundry "fsdf" :slant normal :weight normal :height 135 :width normal)
+                 (t :family :height 100)))
+
+(setq default-frame-alist '((user-size t)
+                            (background . "black")
+                            (foreground . "white")))
 (setq inhibit-startup-message t)
 (setq package-enable-at-startup t)
 (setq package-quickstart t)
