31e7671fecf58a6fc1867b74491094cd750369bd
Juan Fuentes <jj@xmonk.org>
2018-09-19 20:55:33 -0400

    Update and fix a few issues

--- site-lisp/rust-conf.el
+++ site-lisp/rust-conf.el
@@ -27,31 +27,35 @@
 (use-package rust-mode
   :commands rust-mode
   :ensure t
-  :config
-  (setq rustfmt-enable-on-save t)
-  (use-package racer
-    :ensure t
-    :after rust-mode
-    :init
-    (add-hook 'rust-mode-hook #'racer-mode)
-    (add-hook 'racer-mode-hook #'eldoc-mode)
-    (add-hook 'racer-mode-hook #'company-mode)
-    :config
-    (setq racer-cmd (concat (getenv "HOME") "/.cargo/bin/racer"))
-    (setq racer-rust-src-path (concat (getenv "HOME") "/.multirust/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src"))
-    (setq company-tooltip-align-annotations t))
+  :init
+  (setq rustfmt-enable-on-save t))
 
-  (use-package flycheck-rust
-    :ensure t
-    :after rust-mode
-    :init
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+(use-package flycheck-rust
+  :ensure t
+  :after rust-mode
+  :init
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
-  (use-package cargo
-    :ensure t
-    :after rust-mode
-    :init
-    (add-hook 'rust-mode-hook 'cargo-minor-mode)))
+(use-package cargo
+  :ensure t
+  :after rust-mode
+  :init
+  (add-hook 'rust-mode-hook 'cargo-minor-mode))
+
+(use-package racer
+  :ensure t
+  :after rust-mode
+  :init
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode)
+  (setq racer-cmd (concat (getenv "HOME") "/.cargo/bin/racer"))
+  (setq racer-rust-src-path (concat (getenv "HOME") "/.multirust/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src")))
+
+(use-package company-racer
+  :when (file-executable-p (expand-file-name "~/.cargo/bin/racer"))
+  :ensure t
+  :after racer-mode)
 
 (provide 'rust-conf)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
