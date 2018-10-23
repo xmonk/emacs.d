756681740192b423b70759e0c9d9db940c8f5142
Juan Fuentes <jj@xmonk.org>
2018-10-23 16:21:28 -0400

    starting basic js configuration

---
+++ site-lisp/js-conf.el
@@ -0,0 +1,13 @@
+(use-package tern
+  :ensure t
+  :commands tern-mode
+  :init
+  (add-hook 'js-mode-hook (lambda ()
+                            (tern-mode t))))
+
+(use-package company-tern
+  :ensure t
+  :init
+  (add-to-list 'company-backends 'company-tern))
+
+(provide 'js-conf)
