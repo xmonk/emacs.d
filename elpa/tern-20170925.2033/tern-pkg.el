3a7108a196427b0686e0b39aa7cb7d8ae9cb4b37
Juan Fuentes <jj@xmonk.org>
2018-10-23 16:29:46 -0400

    update packages and installing tern, company-tern, docker syntax modules

---
+++ elpa/tern-20170925.2033/tern-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tern" "20170925.2033" "Tern-powered JavaScript integration" '((json "1.2") (cl-lib "0.5") (emacs "24")) :commit "32a8a9877f43bbd0743514816ecbd4afb55fd6d6" :authors '(("Marijn Haverbeke")) :maintainer '("Marijn Haverbeke") :url "http://ternjs.net/")
