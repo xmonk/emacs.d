911cdd72262e6f64dcd33aa8c969aac5215b6f1a
Juan Fuentes <jj@xmonk.org>
2018-11-01 10:17:14 -0400

    Update packages

---
+++ elpa/web-mode-20181031.2027/web-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "web-mode" "20181031.2027" "major mode for editing web templates" '((emacs "23.1")) :commit "2d8c7fc0b337a0b38ed3e5a35a2b9ac23ddafdb7" :keywords '("languages") :authors '(("François-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("François-Xavier Bois") :url "http://web-mode.org")
