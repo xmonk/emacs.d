f1642ac67bc4fd3b11739cc8c4e9b3e820e6927b
Juan Fuentes <jj@xmonk.org>
2018-11-14 13:43:16 -0400

    update packages

---
+++ elpa/git-timemachine-20181114.957/git-timemachine-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-timemachine" "20181114.957" "Walk through git revisions of a file" '((emacs "24.3")) :commit "01fd2a25e8274824c0bc0d0350554f8d43759822" :keywords '("git") :authors '(("Peter Stiernström" . "peter@stiernstrom.se")) :maintainer '("Peter Stiernström" . "peter@stiernstrom.se") :url "https://github.com/pidu/git-timemachine")
