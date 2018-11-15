77a4ec2c22af8915d0dc3521fec4537a47a5a384
Juan Fuentes <jj@xmonk.org>
2018-11-15 19:15:53 -0400

    update packages

---
+++ elpa/git-commit-20181110.1201/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20181110.1201" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "31ba7ea910a421cf6e77598e6cab37e4488f4b4a" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
