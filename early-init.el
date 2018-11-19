96f10c0edb6b6f9ab9f9c6e71b83c93120f68c84
Juan Fuentes <jj@xmonk.org>
2018-11-19 08:23:45 -0400

    Disable ligatures

--- early-init.el
+++ early-init.el
@@ -1,11 +1,11 @@
 (face-spec-set 'default
-               '((((type x)) :family "PragmataPro Mono Liga" :pixelsize 10 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 100 :width normal :scalable t)
-                 (((type ns)) :family "PragmataPro Mono Liga" :pixelsize 10 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
+               '((((type x)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 102 :width normal :scalable t)
+                 (((type ns)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
                  (t :family :height 100)))
 
 (setq default-frame-alist '((user-size t)
-                            (background . "black")
-                            (foreground . "white")))
+                            (background "#1d1f21")
+                            (foreground "#c5c8c6")))
 (setq inhibit-startup-message t)
 (setq package-enable-at-startup t)
 (setq package-quickstart t)
