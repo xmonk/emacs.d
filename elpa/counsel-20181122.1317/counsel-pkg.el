ffe35a99b4e3657dc92aa7edf60539653df8abbd
Juan Fuentes <jj@xmonk.org>
2018-11-23 10:54:43 -0400

    update packages

---
+++ elpa/counsel-20181122.1317/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20181122.1317" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "69856b62de8d1f148ff2a0f1d0a5543171ba5b50" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ elpa/swiper-20181120.1832/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20181120.1832" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "18019278d2b849b1287d299a894a4b98c9707ba2" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
