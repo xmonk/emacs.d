6536bda69a7a6bdec335ad835d0b6e1fd8569693
Juan Fuentes <jj@xmonk.org>
2018-12-14 18:32:05 -0400

    update packages

--- elpa/swiper-20181120.1832/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20181120.1832" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.9.0")) :commit "69856b62de8d1f148ff2a0f1d0a5543171ba5b50" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
--- elpa/whole-line-or-region-20181116.2249/whole-line-or-region-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "whole-line-or-region" "20181116.2249" "operate on current line if region undefined" 'nil :commit "6fcbd6e403a8a66813fdf64ddd3f03b904c82a88" :keywords '("convenience" "wp") :authors '(("Joe Casadonte" . "emacs@northbound-train.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com"))
