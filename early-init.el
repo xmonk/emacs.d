801d079027cf50732b14b67e075505ff398cc959
Juan Fuentes <jj@xmonk.org>
2018-12-20 21:00:55 -0400

    increase font size

--- early-init.el
+++ early-init.el
@@ -1,6 +1,6 @@
 (face-spec-set 'default
                '((((type x)) :family "PragmataPro Mono" :pixelsize 10 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 100 :width normal :scalable t)
-                 (((type ns)) :family "PragmataPro Mono" :pixelsize 10 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
+                 (((type ns)) :family "PragmataPro Mono" :pixelsize 10 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 145 :width normal :scalable t)
                  (t :family :height 100)))
 
 (setq default-frame-alist '((user-size . t)
