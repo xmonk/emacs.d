215f0577ae1d745619abda2180801bdeac087e69
Juan Fuentes <jj@xmonk.org>
2018-12-27 14:06:11 -0400

    cleanup

--- early-init.el
+++ early-init.el
@@ -1,14 +1,6 @@
 (face-spec-set 'default
-               '((((type x)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 102 :width normal :scalable t)
+               '((((type x)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 100 :width normal :scalable t)
                  (((type ns)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
-                 (t :family :height 100)))
-(face-spec-set 'fixed-pitch
-               '((((type x)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 102 :width normal :scalable t)
-                 (((type ns)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
-                 (t :family :height 100)))
-(face-spec-set 'variable-pitch
-               '((((type x)) :family "Lucida Grande" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 100 :width normal :scalable t)
-                 (((type ns)) :family "Lucida Grande" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
                  (t :family :height 100)))
 
 (setq default-frame-alist '((user-size t)
