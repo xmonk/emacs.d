cd44bac9929a9b0f337e309b08f23ad651986b5e
Juan Fuentes <jj@xmonk.org>
2018-12-27 16:14:49 -0400

    pdf-tools tweaks for faster startup

--- site-lisp/pdf-conf.el
+++ site-lisp/pdf-conf.el
@@ -1,32 +1,35 @@
 (use-package pdf-tools
- :pin manual ;;manually update
- :init
- ;; wrapper for save-buffer ignoring arguments
- (defun jj/save-buffer-no-args ()
-   "Save buffer ignoring arguments"
-   (save-buffer))
- (pdf-tools-install)
- (setq-default pdf-view-display-size 'fit-page)
- ;; automatically annotate highlights
- (setq pdf-annot-activate-created-annotations t)
- ;; use isearch instead of swiper
- (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
- ;; turn off cua so copy works
- (add-hook 'pdf-view-mode-hook (lambda () (cua-mode 0)))
- ;; more fine-grained zooming
- (setq pdf-view-resize-factor 1.1)
- ;; keyboard shortcuts
- :config
- (define-key pdf-view-mode-map (kbd "h") 'pdf-annot-add-highlight-markup-annotation)
- (define-key pdf-view-mode-map (kbd "t") 'pdf-annot-add-text-annotation)
- (define-key pdf-view-mode-map (kbd "D") 'pdf-annot-delete)
- ;; wait until map is available
- (use-package pdf-annot
-   :after pdf-tools
-   :config
-   (define-key pdf-annot-edit-contents-minor-mode-map (kbd "<return>") 'pdf-annot-edit-contents-commit)
-   (define-key pdf-annot-edit-contents-minor-mode-map (kbd "<S-return>") 'newline)
-   ;; save after adding comment
-   (advice-add 'pdf-annot-edit-contents-commit :after 'jj/save-buffer-no-args)))
+  :magic ("%PDF" . pdf-view-mode)
+  :mode ("\\.pdf\\'" . pdf-view-mode)
+  :pin manual ;;manually update
+  :init
+  ;; wrapper for save-buffer ignoring arguments
+  (defun jj/save-buffer-no-args ()
+    "Save buffer ignoring arguments"
+    (save-buffer))
+  (pdf-tools-install)
+  (setq-default pdf-view-display-size 'fit-page)
+  ;; automatically annotate highlights
+  (setq pdf-annot-activate-created-annotations t)
+  ;; use isearch instead of swiper
+  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
+  ;; turn off cua so copy works
+  (add-hook 'pdf-view-mode-hook (lambda () (cua-mode 0)))
+  ;; more fine-grained zooming
+  (setq pdf-view-resize-factor 1.1)
+  ;; keyboard shortcuts
+  :config
+  (define-key pdf-view-mode-map (kbd "h") 'pdf-annot-add-highlight-markup-annotation)
+  (define-key pdf-view-mode-map (kbd "t") 'pdf-annot-add-text-annotation)
+  (define-key pdf-view-mode-map (kbd "D") 'pdf-annot-delete)
+  ;; wait until map is available
+  (use-package pdf-annot
+    :requires pdf-tools
+    :mode ("\\.pdf\\'" . pdf-view-mode)
+    :config
+    (define-key pdf-annot-edit-contents-minor-mode-map (kbd "<return>") 'pdf-annot-edit-contents-commit)
+    (define-key pdf-annot-edit-contents-minor-mode-map (kbd "<S-return>") 'newline)
+    ;; save after adding comment
+    (advice-add 'pdf-annot-edit-contents-commit :after 'jj/save-buffer-no-args)))
 
 (provide 'pdf-conf)
