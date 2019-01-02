8bcbc9f4bb025a67dde45fc192d20c3d05f6fa3d
Juan Fuentes <jj@xmonk.org>
2019-01-02 08:08:13 -0400

    update packages

---
+++ elpa/deadgrep-20181229.1348/deadgrep-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "deadgrep" "20181229.1348" "fast, friendly searching with ripgrep" '((emacs "25.1") (dash "2.12.0") (s "1.11.0") (spinner "1.7.3")) :commit "b663a8f27513ab68081e2bb46402b74834b5ad65" :keywords '("tools") :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk") :url "https://github.com/Wilfred/deadgrep")
