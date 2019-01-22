128a05f105ba136f6725d38b9c9d0eaaac18030d
Juan Fuentes <jj@xmonk.org>
2019-01-22 09:20:58 -0400

    update packages

---
+++ elpa/avy-20190122.1159/avy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "avy" "20190122.1159" "Jump to arbitrary positions in visible text and select text quickly." '((emacs "24.1") (cl-lib "0.5")) :commit "5b3676f1a4212b43de33e8c7a8f3a2de933068c7" :keywords '("point" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/avy")
