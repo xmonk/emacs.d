04ac62ec08445cf49df972a61566678bec03fd1e
Juan Fuentes <jj@xmonk.org>
2019-02-19 09:22:45 -0400

    remove accidentally commited file

--- transient/history.el
+++
@@ -1,9 +0,0 @@
-((magit-branch nil)
- (magit-commit nil)
- (magit-diff
-  ("--diff-algorithm=histogram" "--no-ext-diff" "--stat"))
- (magit-diff-refresh
-  ("--diff-algorithm=histogram" "--no-ext-diff"))
- (magit-diff:--diff-algorithm)
- (magit-dispatch nil)
- (magit-push nil))
