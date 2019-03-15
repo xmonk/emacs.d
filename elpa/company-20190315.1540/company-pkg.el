5866aa2dc27f13d4ea3600ad352980a48fdc0b86
Juan Fuentes <jj@xmonk.org>
2019-03-15 13:23:57 -0400

    package update

--- elpa/company-20190315.1540/company-pkg.el
+++ elpa/company-20190315.1540/company-pkg.el
@@ -1,4 +1,4 @@
-(define-package "company" "20190221.1427" "Modular text completion framework"
+(define-package "company" "20190315.1540" "Modular text completion framework"
   '((emacs "24.3"))
   :keywords
   '("abbrev" "convenience" "matching")
