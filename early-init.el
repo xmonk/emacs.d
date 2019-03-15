1ad5ba19349d995aeb1f4315340c86d5fb67c200
Juan Fuentes <jj@xmonk.org>
2019-03-15 09:10:47 -0400

    going back to lucida

--- early-init.el
+++ early-init.el
@@ -1,6 +1,6 @@
 (face-spec-set 'default
-               '((((type x)) :family "PragmataPro" :pixelsize 100 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 105 :width normal :scalable t)
-                 (((type ns)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
+               '((((type x)) :family "Lucida Grande Mono" :pixelsize 100 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 100 :width normal :scalable t)
+                 (((type ns)) :family "Lucida Grande Mono" :pixelsize 200 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
                  (t :family :height 100)))
 
 (setq default-frame-alist '((user-size t)
