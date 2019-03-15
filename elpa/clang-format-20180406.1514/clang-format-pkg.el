fe0661aea8f1120262c26f5691e3b115bd00b9ab
Juan Fuentes <jj@xmonk.org>
2019-03-15 09:11:22 -0400

    update packages

---
+++ elpa/clang-format-20180406.1514/clang-format-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/jfuentes/.emacs.d/elpa/clang-format-20180406.1514/clang-format.el  -*- no-byte-compile: t -*-
+(define-package "clang-format" "20180406.1514" "Format code using clang-format" '((cl-lib "0.3")) :commit "5a39b0b5bd954f0f40bb997bc4e6f74d69dad220" :keywords '("tools" "c"))
