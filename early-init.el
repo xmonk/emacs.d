0bfb0935a6cfc07942448a1777f91d4d6ea9e573
Juan Fuentes <jj@xmonk.org>
2019-03-20 09:49:42 -0400

    tweaking different font options..

--- early-init.el
+++ early-init.el
@@ -1,4 +1,14 @@
 (face-spec-set 'default
+               '((((type x)) :family "PragmataPro Mono" :pixelsize 100 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 105 :width normal :scalable t)
+                 (((type ns)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
+                 (t :family :height 100)))
+
+(face-spec-set 'fix-pitch
+               '((((type x)) :family "Lucida Grande Mono" :pixelsize 100 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 100 :width normal :scalable t)
+                 (((type ns)) :family "Lucida Grande Mono" :pixelsize 200 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
+                 (t :family :height 100)))
+
+(face-spec-set 'variable-pitch
                '((((type x)) :family "Lucida Grande Mono" :pixelsize 100 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 100 :width normal :scalable t)
                  (((type ns)) :family "Lucida Grande Mono" :pixelsize 200 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
                  (t :family :height 100)))
