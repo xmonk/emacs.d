6503fcaa5d0bb5583e84448defcb911ff542728c
Juan Fuentes <jj@xmonk.org>
2019-03-26 09:02:34 -0400

    Update packages, removed eglot for lsp-mode

--- elpa/ace-window-20190325.1103/ace-window-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/jfuentes/.emacs.d/elpa/ace-window-20190325.1103/ace-window.el  -*- no-byte-compile: t -*-
-(define-package "ace-window" "20190325.1103" "Quickly switch windows." '((avy "0.2.0")) :commit "2d132bab253de7c09500881b4302f47d15584253" :keywords '("window" "location") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/ace-window")
---
+++ elpa/counsel-20190326.940/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/jfuentes/.emacs.d/elpa/counsel-20190326.940/counsel.el  -*- no-byte-compile: t -*-
+(define-package "counsel" "20190326.940" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :commit "b154260a26d8927b1dc11d2753b6d1ac552d366a" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
