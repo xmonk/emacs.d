5b8db44badcbf6c8d03fbcdf730f3469153978f2
Juan Fuentes <jj@xmonk.org>
2019-03-26 10:48:05 -0400

    configure lsp-mode

--- site-lisp/lsp-conf.el
+++ site-lisp/lsp-conf.el
@@ -3,8 +3,15 @@
 (use-package lsp-mode
   :ensure t
   :commands lsp
+  :hook ((python-mode bash-mode) . lsp)
   :init
-  (add-hook 'python-mode-hook #'lsp))
+  (setq lsp-auto-guess-root t)
+  (setq lsp-prefer-flymake nil)
+  :config
+  (require 'lsp-clients)
+  (define-key lsp-mode-map (kbd "M-.") 'lsp-find-definition)
+  (define-key lsp-mode-map (kbd "M-r") 'lsp-find-references)
+  (define-key lsp-mode-map (kbd "S-r") 'lsp-rename))
 
 (use-package lsp-ui
   :ensure t
@@ -17,5 +24,12 @@
   (setq company-lsp-enable-recompletion t)
   (setq company-lsp-async t)
   (setq company-lsp-cache-candidates t))
+
+(use-package dap-mode
+  :ensure t
+  :config
+  (dap-mode 1)
+  (dap-ui-mode 1)
+  (require 'dap-python))
 
 (provide 'lsp-conf)
