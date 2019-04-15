682fed0521f3c84f0c3a959d9bfa4ad596e6047e
Juan Fuentes <jj@xmonk.org>
2019-04-15 09:19:17 -0400

    update packages

--- elpa/swiper-20190409.1035/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/jfuentes/.emacs.d/elpa/swiper-20190409.1035/swiper.el  -*- no-byte-compile: t -*-
-(define-package "swiper" "20190409.1035" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :commit "44ea8b5edf7115269db21987ba812bb68e4b078b" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
