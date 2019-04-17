37845d8559a226bcd5c69309755128a768425270
Juan Fuentes <jj@xmonk.org>
2019-04-17 14:31:21 -0400

    enhance company configuration based on centaur

--- site-lisp/company-conf.el
+++ site-lisp/company-conf.el
@@ -1,11 +1,23 @@
-;;; company
+;;; company -*- lexical-binding: t; -*-
 (use-package company
   :ensure t
   :commands (company-mode company-complete)
-  :bind (("C-c TAB" . company-complete-common-or-cycle))
+  :bind (("C-c TAB" . company-complete)
+         ("<backtab>" . company-yasnippet)
+         :map company-active-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next)
+         ("<tab>" . company-complete-common-or-cycle)
+         ("<backtab>" . my-company-yasnippet)
+         ;; ("C-c C-y" . my-company-yasnippet)
+         :map company-search-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next))
+
   :diminish company-mode
   :init
   (setq company-tooltip-align-annotations t)
+  (setq company-require-match nil)
   :config
   (global-company-mode 1))
 
@@ -23,5 +35,13 @@
   (add-hook 'company-mode-hook (lambda () (add-to-list 'company-backends 'company-capf)))
   :config
   (company-flx-mode +1))
+
+(use-package company-quickhelp
+  :ensure t
+  :defines company-quickhelp-delay
+  :bind (:map company-active-map
+              ("M-h" . company-quickhelp-manual-begin))
+  :hook (global-company-mode . company-quickhelp-mode)
+  :init (setq company-quickhelp-delay 0.8))
 
 (provide 'company-conf)
