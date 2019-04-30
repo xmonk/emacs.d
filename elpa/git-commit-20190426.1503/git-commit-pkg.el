11b26325bce3b62c9eed5a4c557df84fb1802e1b
Juan Fuentes <jj@xmonk.org>
2019-04-30 14:25:30 -0400

    update packages

---
+++ elpa/git-commit-20190426.1503/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jj/.emacs.d/elpa/git-commit-20190426.1503/git-commit.el  -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190426.1503" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "b4d6320d0c5ac6777802b2ede84701958dc43f98" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
