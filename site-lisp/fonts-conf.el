5e91841ccc4a4a1aa28a69c8b826a225b56affde
Juan Fuentes <jj@xmonk.org>
2019-05-07 10:01:50 -0400

    moved pragmatapro symbols to vendor

--- site-lisp/fonts-conf.el
+++ site-lisp/fonts-conf.el
@@ -1,5 +1,18 @@
-(load-file (concat user-emacs-directory "site-lisp" "/" "pragmatapro-prettify-symbols-v0.827.el"))
+;; -*- lexical-binding: t; -*-
+
+(load-library (concat user-emacs-directory "vendor" "/" "pragmatapro-prettify-symbols-v0.827.el"))
 (add-hook 'prog-mode-hook #'add-pragmatapro-prettify-symbols-alist)
+(add-hook 'markdown-mode-hook #'add-pragmatapro-prettify-symbols-alist)
+(add-hook 'text-mode-hook #'add-pragmatapro-prettify-symbols-alist)
+
 (global-prettify-symbols-mode +1)
+
+;; (use-package pragmata-pro
+;;   :load-path "vendor/pragmata-pro/"
+;;   :diminish
+;;   :config
+;;   (pragmata-pro-mode)
+;;   (global-prettify-symbols-mode +1))
+
 
 (provide 'fonts-conf)
