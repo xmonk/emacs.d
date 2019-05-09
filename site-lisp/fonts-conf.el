9a124386823273eb892fe3aabc844109f739275d
Juan Fuentes <jj@xmonk.org>
2019-05-09 08:53:23 -0400

    use a more emacs package friendly pragmatapro ligature file

--- site-lisp/fonts-conf.el
+++ site-lisp/fonts-conf.el
@@ -1,18 +1,12 @@
 ;; -*- lexical-binding: t; -*-
 
-(load-library (concat user-emacs-directory "vendor" "/" "pragmatapro-prettify-symbols-v0.827.el"))
-(add-hook 'prog-mode-hook #'add-pragmatapro-prettify-symbols-alist)
-(add-hook 'markdown-mode-hook #'add-pragmatapro-prettify-symbols-alist)
-(add-hook 'text-mode-hook #'add-pragmatapro-prettify-symbols-alist)
-
-(global-prettify-symbols-mode +1)
-
-;; (use-package pragmata-pro
-;;   :load-path "vendor/pragmata-pro/"
-;;   :diminish
-;;   :config
-;;   (pragmata-pro-mode)
-;;   (global-prettify-symbols-mode +1))
+;; git@github.com:DeLaGuardo/pragmata-pro.el.git
+(use-package pragmata-pro
+  :load-path "vendor/pragmata-pro/"
+  :diminish
+  :config
+  (pragmata-pro-mode)
+  (global-prettify-symbols-mode +1))
 
 
 (provide 'fonts-conf)
