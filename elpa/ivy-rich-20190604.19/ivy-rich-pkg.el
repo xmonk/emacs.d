628b17b7b2606ffdc34ead24f2d7a92364de3242
Juan Fuentes <jj@xmonk.org>
2019-06-04 09:43:23 -0400

    package version bump

---
+++ elpa/ivy-rich-20190604.19/ivy-rich-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/jfuentes/.emacs.d/elpa/ivy-rich-20190604.19/ivy-rich.el  -*- no-byte-compile: t -*-
+(define-package "ivy-rich" "20190604.19" "More friendly display transformer for ivy." '((emacs "24.4") (ivy "0.8.0")) :commit "f6bfa293c6df0b43cc411876b665816ec3f03d08" :keywords '("ivy") :authors '(("Yevgnen Koh" . "wherejoystarts@gmail.com")) :maintainer '("Yevgnen Koh" . "wherejoystarts@gmail.com"))
