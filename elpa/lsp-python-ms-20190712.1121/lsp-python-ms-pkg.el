dd6348fb861429c117d9229191e0a9b3d1652091
Juan Fuentes <jj@xmonk.org>
2019-07-25 08:12:21 -0400

    packages

--- elpa/lsp-python-ms-20190712.1121/lsp-python-ms-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/jfuentes/.emacs.d/elpa/lsp-python-ms-20190712.1121/lsp-python-ms.el  -*- no-byte-compile: t -*-
-(define-package "lsp-python-ms" "20190712.1121" "lsp-mode client for Microsoft python-language-server" '((cl-lib "0.6.1") (lsp-mode "6.0") (python "0.26.1") (json "1.4") (emacs "24.4")) :commit "cbe41dfbfea9d75badbd86a44dfbad6eaaa9a5a1" :keywords '("languages" "tools") :authors '(("Charl Botha")) :maintainer '("Andrew Christianson") :url "https://github.com/andrew-christianson/lsp-python-ms")
