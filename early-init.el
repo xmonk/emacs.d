8592bf90a955c58585e638a49cdcdead3ae4a5ed
Juan Fuentes <jj@xmonk.org>
2019-08-02 18:14:00 -0400

    package version bump

--- early-init.el
+++ early-init.el
@@ -6,16 +6,6 @@
                  (((type ns)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 145 :width normal :scalable t)
                  (t :family :height 100)))
 
-(face-spec-set 'fix-pitch
-               '((((type x)) :family "Lucida Grande Mono" :pixelsize 100 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 100 :width normal :scalable t)
-                 (((type ns)) :family "Lucida Grande Mono" :pixelsize 200 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
-                 (t :family :height 100)))
-
-(face-spec-set 'variable-pitch
-               '((((type x)) :family "Lucida Grande" :pixelsize 100 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 100 :width normal :scalable t)
-                 (((type ns)) :family "Lucida Grande" :pixelsize 200 :foundry "B&H" :slant normal :weight normal :spacing 100 :height 135 :width normal :scalable t)
-                 (t :family :height 100)))
-
 (setq default-frame-alist '((user-size t)
                             (background "#1d1f21")
                             (foreground "#c5c8c6")))
