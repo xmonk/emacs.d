65895ecc243f79d1e9f6ee6938f80cb011253963
Juan Fuentes <jj@xmonk.org>
2019-08-09 07:46:35 -0400

    package version bump

---
+++ elpa/go-rename-20190805.2101/go-rename-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/jfuentes/.emacs.d/elpa/go-rename-20190805.2101/go-rename.el  -*- no-byte-compile: t -*-
+(define-package "go-rename" "20190805.2101" "Integration of the 'gorename' tool into Emacs." '((go-mode "1.3.1")) :commit "ac6dd8c1f4b969fb93ecb8ac62b0700eeb1b62fd" :keywords '("tools"))
