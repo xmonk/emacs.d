479b18ee1971675605d10eab436ed8f235b3f7af
Juan Fuentes <jj@xmonk.org>
2019-08-08 20:11:00 -0400

    Package version bump and install vterm

---
+++ elpa/go-mode-20190807.2229/go-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/jj/.emacs.d/elpa/go-mode-20190807.2229/go-mode.el  -*- no-byte-compile: t -*-
+(define-package "go-mode" "20190807.2229" "Major mode for the Go programming language" 'nil :commit "1fcf76d47e33c78aa58e7470f443ac438b753a83" :keywords '("languages" "go") :authors '(("The go-mode Authors")) :maintainer '("The go-mode Authors") :url "https://github.com/dominikh/go-mode.el")
