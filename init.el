148c14f8559a7aded2c703621c4c22a48eaae3ba
Juan Fuentes <jj@xmonk.org>
2019-10-23 19:09:02 -0400

    Streamlined init.el
    Removed superflous checks that are already done in early-init.el

--- init.el
+++ init.el
@@ -28,26 +28,13 @@
 (add-to-list 'custom-theme-load-path (expand-file-name (concat user-emacs-directory "themes/")))
 (add-to-list 'custom-theme-load-path (expand-file-name (concat user-emacs-directory "themes/naysayer-theme")))
 (add-to-list 'custom-theme-load-path (expand-file-name (concat user-emacs-directory "themes/tron-legacy-emacs-theme")))
-
-(unless (window-system)
-  (menu-bar-mode -1)
-  (load-theme 'jj-term t))
-
-(when (window-system)
-  (load-theme 'jj-dark t)
-  (setenv "RIPGREP_CONFIG_PATH" (concat (getenv "HOME") "/.ripgreprc"))
-  (setenv "TMPDIR" "/tmp"))
+(load-theme 'jj-dark t)
 
 ;; Load customization's
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (if (file-exists-p custom-file)
     (load-file custom-file)
   (message (format "ERROR: custom file: %s not found or not specified" custom-file)))
-
-;;; Server
-(require 'server nil t)
-(when (and (>= emacs-major-version 23) (not (server-running-p)))
-  (server-start))
 
 (add-to-list 'load-path (expand-file-name (concat user-emacs-directory "/site-lisp")))
 (use-package defuns)
@@ -63,6 +50,11 @@
 (use-package company-conf :defer 0.5)
 (use-package lsp-conf :defer 0.5)
 (use-package vterm-conf)
+
+;;; Server
+(require 'server nil t)
+(when (and (>= emacs-major-version 23) (not (server-running-p)))
+  (server-start))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init.el ends here
