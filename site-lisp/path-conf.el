3c1fff6eb0aa38892720ad7343ecc810d70d82fd
Juan Fuentes <jj@xmonk.org>
2020-02-26 11:30:05 -0400

    Shell arguments does not do anything useful here

--- site-lisp/path-conf.el
+++ site-lisp/path-conf.el
@@ -5,9 +5,9 @@
     :ensure t
     :defer
     :init
+    (setq exec-path-from-shell-debug t)
     (setq exec-path-from-shell-check-startup-files nil)
     (setq exec-path-from-shell-variables '("PATH" "MANPATH" "PYTHONPATH" "GOPATH" "RUST_SRC_PATH" "RIPGREP_CONFIG_PATH"))
-    (setq exec-path-from-shell-arguments '("-l"))
     (exec-path-from-shell-initialize)))
 
 (provide 'path-conf)
