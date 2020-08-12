53a144308c10d63d9a4aa5a596ca7586ef04f7ff
Juan Fuentes <jj@xmonk.org>
2020-08-12 13:58:25 -0400

    font addjustment

--- early-init.el
+++ early-init.el
@@ -34,12 +34,14 @@
 ;;; Fonts
 (face-spec-set 'default
                '((((type x)) :family "PragmataPro Mono" :pixelsize 100 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 105 :width normal :scalable t)
-                 (((type ns)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 145 :width normal :scalable t)
+                 (((type ns)) :family "PragmataPro Mono" :pixelsize 200 :foundry "FSD" :slant normal :weight normal :spacing 100 :height 155 :width normal :scalable t)
                  (t :family :height 100)))
 
 (setq default-frame-alist '((user-size t)
                             (background "#1d1f21")
                             (foreground "#c5c8c6")))
+(setq frame-resize-pixelwise t)
+
 ;;; package
 (require 'package nil t)
 (setq package-archives
