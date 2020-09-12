f5e6738def5667ef6a675d56d84f5098ed11f45d
Juan Fuentes <jj@xmonk.org>
2020-09-12 12:11:17 -0400

    Fix issues whith loading env variables.
    If a user doesn't have a .zshenv or a .profile file the env variables
    won't be loaded.

--- site-lisp/path-conf.el
+++ site-lisp/path-conf.el
@@ -3,10 +3,7 @@
 (when (memq window-system '(mac ns x))
   (use-package exec-path-from-shell
     :ensure t
-    :defer
     :init
-    ;; (setq exec-path-from-shell-check-startup-files nil)
-    (setq exec-path-from-shell-arguments '("-l"))
     (setq exec-path-from-shell-variables '("PATH" "MANPATH" "PYTHONPATH" "GOPATH" "RUST_SRC_PATH" "RIPGREP_CONFIG_PATH" "WORKON_HOME"))
     (exec-path-from-shell-initialize)))
 
